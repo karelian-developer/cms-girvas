4cac21ac646538351978faade2099bf1bfb30dbc
Drelagas <drelagas.new@gmail.com>
2025-05-16 17:20:47 +0300

    - Мелкие правки;

--- api/pageStatic/patch.handler.php
+++ api/pageStatic/patch.handler.php
@@ -110,7 +110,7 @@ if ($system_core->client->is_logged(2)) {
             $page_static_data['metadata']['additionalFields'][$field_name_transformed] = htmlspecialchars(str_replace('\'', '"', $value));
           }
 
-          if ($key === 'page_static_template_path') {
+          if ($name === 'page_static_template_path') {
             $page_static_data['metadata']['personalTemplatePath'] = htmlspecialchars(str_replace('\'', '"', trim($value)));
           }
         }
