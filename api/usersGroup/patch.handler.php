107442d323a69e3e1ab6ed916300e20d4bfb1565
Drelagas <drelagas.new@gmail.com>
2025-06-29 21:06:44 +0300

    0.1.36-1.76 - Добавлена проверка на наличие группы пользователем с таким же техническим наименованием, как и у редактируемой;

--- api/usersGroup/patch.handler.php
+++ api/usersGroup/patch.handler.php
@@ -26,69 +26,75 @@ if ($CMSCore->client->isLogged(2)) {
   if ($clientUserGroup->permissionCheck($clientUserGroup::PERMISSION_ADMIN_USERS_GROUPS_MANAGEMENT)) {
     if (isset($_PATCH['user_group_id'])) {
       $usersGroupID = is_numeric($_PATCH['user_group_id']) ? (int) $_PATCH['user_group_id'] : 0;
+      $userGroupName = isset($_PATCH['user_group_name']) ? urlencode(htmlentities($_PATCH['user_group_name'])) : '';
       
       if (UserGroup::existsByID($CMSCore, $usersGroupID)) {
-        $usersGroup = new UserGroup($CMSCore, $usersGroupID);
-        $usersGroupData = [];
+        if (!UserGroup::existsByName($CMSCore, $userGroupName)) {
+          $usersGroup = new UserGroup($CMSCore, $usersGroupID);
+          $usersGroupData = [];
 
-        $usersGroupPermissions = 0x0000000000000000;
-        $usersGroupPermissionsArray = $_PATCH['user_group_permissions'] ?? [];
-        
-        if (!empty($usersGroupPermissionsArray)) {
-          foreach ($usersGroupPermissionsArray as $permission) {
-            $usersGroupPermissions = match ($permission) {
-              'admin_panel_auth' => $usersGroupPermissions | UserGroup::PERMISSION_ADMIN_PANEL_AUTH,
-              'admin_users_management' => $usersGroupPermissions | UserGroup::PERMISSION_ADMIN_USERS_MANAGEMENT,
-              'admin_users_groups_management' => $usersGroupPermissions | UserGroup::PERMISSION_ADMIN_USERS_GROUPS_MANAGEMENT,
-              'admin_modules_management' => $usersGroupPermissions | UserGroup::PERMISSION_ADMIN_MODULES_MANAGEMENT,
-              'admin_templates_management' => $usersGroupPermissions | UserGroup::PERMISSION_ADMIN_TEMPLATES_MANAGEMENT,
-              'admin_settings_management' => $usersGroupPermissions | UserGroup::PERMISSION_ADMIN_SETTINGS_MANAGEMENT,
-              'admin_feeds_management' => $usersGroupPermissions | UserGroup::PERMISSION_ADMIN_FEEDS_MANAGEMENT,
-              'admin_viewing_logs' => $usersGroupPermissions | UserGroup::PERMISSION_ADMIN_VIEWING_LOGS,
-              'moder_users_ban' => $usersGroupPermissions | UserGroup::PERMISSION_MODER_USERS_BAN,
-              'moder_entries_comments_management' => $usersGroupPermissions | UserGroup::PERMISSION_MODER_ENTRIES_COMMENTS_MANAGEMENT,
-              'moder_users_warns' => $usersGroupPermissions | UserGroup::PERMISSION_MODER_USERS_WARNS,
-              'editor_media_files_management' => $usersGroupPermissions | UserGroup::PERMISSION_EDITOR_MEDIA_FILES_MANAGEMENT,
-              'editor_entries_edit' => $usersGroupPermissions | UserGroup::PERMISSION_EDITOR_ENTRIES_EDIT,
-              'editor_entries_categories_edit' => $usersGroupPermissions | UserGroup::PERMISSION_EDITOR_ENTRIES_CATEGORIES_EDIT,
-              'editor_pages_static_edit' => $usersGroupPermissions | UserGroup::PERMISSION_EDITOR_PAGES_STATIC_EDIT,
-              'base_entry_comment_create' => $usersGroupPermissions | UserGroup::PERMISSION_BASE_ENTRY_COMMENT_CREATE,
-              'base_entry_comment_change' => $usersGroupPermissions | UserGroup::PERMISSION_BASE_ENTRY_COMMENT_CHANGE,
-              'base_entry_comment_rate' => $usersGroupPermissions | UserGroup::PERMISSION_BASE_ENTRY_COMMENT_RATE,
-            };
-          }
-        }
-
-        $usersGroupData['permissions'] = $usersGroupPermissions;
-
-        $CMSLocalesNames = $CMSCore->getArrayLocalesNames();
-        if (count($CMSLocalesNames) > 0) {
-          foreach ($CMSLocalesNames as $index => $name) {
-            $CMSLocale = new Locale($CMSCore, $name);
-            $CMSLocaleName = $CMSLocale->getName();
-
-            $usersGroupTitleInputName = 'user_group_title_' . $CMSLocale->getISO639(2);
-
-            if (!array_key_exists('metadata', $usersGroupData)) $usersGroupData['metadata'] = [];
-
-            if (array_key_exists($usersGroupTitleInputName, $_PATCH)) {
-              if (!array_key_exists('texts', $usersGroupData)) $usersGroupData['texts'] = [];
-              if (!array_key_exists($CMSLocaleName, $usersGroupData['texts'])) $usersGroupData['texts'][$CMSLocaleName] = [];
-
-              if (array_key_exists($usersGroupTitleInputName, $_PATCH)) $usersGroupData['texts'][$CMSLocaleName]['title'] = htmlspecialchars(str_replace('\'', '"', $_PATCH[$usersGroupTitleInputName]));
+          $usersGroupPermissions = 0x0000000000000000;
+          $usersGroupPermissionsArray = $_PATCH['user_group_permissions'] ?? [];
+          
+          if (!empty($usersGroupPermissionsArray)) {
+            foreach ($usersGroupPermissionsArray as $permission) {
+              $usersGroupPermissions = match ($permission) {
+                'admin_panel_auth' => $usersGroupPermissions | UserGroup::PERMISSION_ADMIN_PANEL_AUTH,
+                'admin_users_management' => $usersGroupPermissions | UserGroup::PERMISSION_ADMIN_USERS_MANAGEMENT,
+                'admin_users_groups_management' => $usersGroupPermissions | UserGroup::PERMISSION_ADMIN_USERS_GROUPS_MANAGEMENT,
+                'admin_modules_management' => $usersGroupPermissions | UserGroup::PERMISSION_ADMIN_MODULES_MANAGEMENT,
+                'admin_templates_management' => $usersGroupPermissions | UserGroup::PERMISSION_ADMIN_TEMPLATES_MANAGEMENT,
+                'admin_settings_management' => $usersGroupPermissions | UserGroup::PERMISSION_ADMIN_SETTINGS_MANAGEMENT,
+                'admin_feeds_management' => $usersGroupPermissions | UserGroup::PERMISSION_ADMIN_FEEDS_MANAGEMENT,
+                'admin_viewing_logs' => $usersGroupPermissions | UserGroup::PERMISSION_ADMIN_VIEWING_LOGS,
+                'moder_users_ban' => $usersGroupPermissions | UserGroup::PERMISSION_MODER_USERS_BAN,
+                'moder_entries_comments_management' => $usersGroupPermissions | UserGroup::PERMISSION_MODER_ENTRIES_COMMENTS_MANAGEMENT,
+                'moder_users_warns' => $usersGroupPermissions | UserGroup::PERMISSION_MODER_USERS_WARNS,
+                'editor_media_files_management' => $usersGroupPermissions | UserGroup::PERMISSION_EDITOR_MEDIA_FILES_MANAGEMENT,
+                'editor_entries_edit' => $usersGroupPermissions | UserGroup::PERMISSION_EDITOR_ENTRIES_EDIT,
+                'editor_entries_categories_edit' => $usersGroupPermissions | UserGroup::PERMISSION_EDITOR_ENTRIES_CATEGORIES_EDIT,
+                'editor_pages_static_edit' => $usersGroupPermissions | UserGroup::PERMISSION_EDITOR_PAGES_STATIC_EDIT,
+                'base_entry_comment_create' => $usersGroupPermissions | UserGroup::PERMISSION_BASE_ENTRY_COMMENT_CREATE,
+                'base_entry_comment_change' => $usersGroupPermissions | UserGroup::PERMISSION_BASE_ENTRY_COMMENT_CHANGE,
+                'base_entry_comment_rate' => $usersGroupPermissions | UserGroup::PERMISSION_BASE_ENTRY_COMMENT_RATE,
+              };
             }
           }
-        }
 
-        if (isset($_PATCH['user_group_name'])) $usersGroupData['name'] = urlencode(htmlentities($_PATCH['user_group_name']));
+          $usersGroupData['permissions'] = $usersGroupPermissions;
 
-        $usersGroupIsUpdated = $usersGroup->update($usersGroupData);
+          $CMSLocalesNames = $CMSCore->getArrayLocalesNames();
+          if (count($CMSLocalesNames) > 0) {
+            foreach ($CMSLocalesNames as $index => $name) {
+              $CMSLocale = new Locale($CMSCore, $name);
+              $CMSLocaleName = $CMSLocale->getName();
 
-        if ($usersGroupIsUpdated) {
-          $handlerMessage = $handlerMessage ?? $CMSCore->locale->getSingleValueByKey('API_PATCH_DATA_SUCCESS');
-          $handlerStatusCode = $handlerStatusCode ?? 1;
+              $usersGroupTitleInputName = 'user_group_title_' . $CMSLocale->getISO639(2);
+
+              if (!array_key_exists('metadata', $usersGroupData)) $usersGroupData['metadata'] = [];
+
+              if (array_key_exists($usersGroupTitleInputName, $_PATCH)) {
+                if (!array_key_exists('texts', $usersGroupData)) $usersGroupData['texts'] = [];
+                if (!array_key_exists($CMSLocaleName, $usersGroupData['texts'])) $usersGroupData['texts'][$CMSLocaleName] = [];
+
+                if (array_key_exists($usersGroupTitleInputName, $_PATCH)) $usersGroupData['texts'][$CMSLocaleName]['title'] = htmlspecialchars(str_replace('\'', '"', $_PATCH[$usersGroupTitleInputName]));
+              }
+            }
+          }
+
+          if (isset($_PATCH['user_group_name'])) $usersGroupData['name'] = urlencode(htmlentities($_PATCH['user_group_name']));
+
+          $usersGroupIsUpdated = $usersGroup->update($usersGroupData);
+
+          if ($usersGroupIsUpdated) {
+            $handlerMessage = $handlerMessage ?? $CMSCore->locale->getSingleValueByKey('API_PATCH_DATA_SUCCESS');
+            $handlerStatusCode = $handlerStatusCode ?? 1;
+          } else {
+            $handlerMessage = $handlerMessage ?? 'API ERROR: ' . $CMSCore->locale->getSingleValueByKey('API_ERROR_UNKNOWN');
+            $handlerStatusCode = $handlerStatusCode ?? 0;
+          }
         } else {
-          $handlerMessage = $handlerMessage ?? 'API ERROR: ' . $CMSCore->locale->getSingleValueByKey('API_ERROR_UNKNOWN');
+          $handlerMessage = $handlerMessage ?? 'API ERROR: ' .$CMSCore->locale->getSingleValueByKey('API_USERS_GROUP_ERROR_NAME_ALREADY_EXISTS');
           $handlerStatusCode = $handlerStatusCode ?? 0;
         }
       } else {
