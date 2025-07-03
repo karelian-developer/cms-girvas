e86a72d5e15b286e85de6d37bf9547c2589126bf
Drelagas <drelagas.new@gmail.com>
2025-07-03 22:59:24 +0300

    0.1.36-16.16 - Мелкие исправления;

--- core/PHPLibrary/Page/Admin/feeds.class.php
+++ core/PHPLibrary/Page/Admin/feeds.class.php
@@ -99,7 +99,7 @@ class PageFeeds implements InterfacePage
     unset($feeds);
 
     foreach ($feedsObjects as $index => $object) {
-      $object->initData(['id', 'name', 'type_id', 'texts', 'createdUnixTimestamp', 'updatedUnixTimestamp']);
+      $object->initData(['id', 'name', 'typeID', 'texts', 'createdUnixTimestamp', 'updatedUnixTimestamp']);
 
       $createdUnixTimestamp = date('d.m.Y H:i:s', $object->getCreatedUnixTimestamp());
       $updatedUnixTimestamp = date('d.m.Y H:i:s', $object->getUpdatedUnixTimestamp());
