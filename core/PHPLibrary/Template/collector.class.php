1ed7fec47d3bc4fe79f350eeb7d4b96a2db65bdb
Drelagas <drelagas.new@gmail.com>
2025-07-07 09:07:23 +0300

    0.1.36-19.1 - Мелкие исправления;

--- core/PHPLibrary/Template/collector.class.php
+++ core/PHPLibrary/Template/collector.class.php
@@ -134,7 +134,7 @@ final class Collector
     if (!empty($localeData)) {
       foreach ($localeData as $name => $value) {
         if (preg_match(self::TEMPLATE_TAG_LANG_PATTERN, $themeTransformed)) {
-          $themeTransformed = strtr($themeTransformed, "{LANG:{$name}}", $value);
+          $themeTransformed = str_replace("{LANG:{$name}}", $value, $themeTransformed);
         }
       }
     }
@@ -170,7 +170,7 @@ final class Collector
             $parsedown->setMarkupEscaped(true);
 
             $fileMarkdownContent = file_get_contents($fileMarkdownPath);
-            $themeTransformed = strtr($themeTransformed, "{LANG:MD:{$name}}", $parsedown->text($fileMarkdownContent));
+            $themeTransformed = str_replace("{LANG:MD:{$name}}", $parsedown->text($fileMarkdownContent), $themeTransformed);
           }
         }
       }
@@ -198,7 +198,7 @@ final class Collector
 
     foreach($variables as $name => $value) {
       if (preg_match(self::TEMPLATE_TAG_PATTERN, $template)) {
-        $template = strtr($template, "{{$name}}", $value);
+        $template = str_replace("{{$name}}", $value, $template);
       }
     }
 
@@ -235,7 +235,7 @@ final class Collector
       if ($matches[2] == '<') $defineFunctionReturned = $defineFunction($matches[1]) < $matches[3];
 
       if ($defineFunctionReturned) {
-        $themeTransformed = strtr($themeTransformed, $matches[0], self::assemblyLogic($matches[4]));
+        $themeTransformed = str_replace($matches[0], self::assemblyLogic($matches[4]), $themeTransformed);
       } else {
         $themeTransformed = '';
       }
