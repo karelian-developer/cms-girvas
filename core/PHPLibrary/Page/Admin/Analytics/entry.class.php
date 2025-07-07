ed10b92e9e85c60a19405195f92e4aa94d7f847f
Drelagas <drelagas.new@gmail.com>
2025-07-07 13:15:25 +0300

    0.1.36-19.6 - Мелкие исправления;

--- core/PHPLibrary/Page/Admin/Analytics/entry.class.php
+++ core/PHPLibrary/Page/Admin/Analytics/entry.class.php
@@ -52,30 +52,11 @@ class PageEntry implements InterfacePage
     $localeData = $this->CMSCore->locale->getData();
     $localeName = $this->CMSCore->locale->getName();
 
-    /** @var array Преобразованные элементы навигации */
-    $navigationsItemsTransformed = [];
-    array_push($navigationsItemsTransformed, ThemeCollector::assemblyFileContent($this->CMSCore->theme, 'templates/page/navigationHorizontal/item.tpl', [
-      'NAVIGATION_ITEM_TITLE' => '< {LANG:PAGE_ENTRY_NAVIGATION_BACK_LABEL}',
-      'NAVIGATION_ITEM_URL' => '/admin/entries',
-      'NAVIGATION_ITEM_LINK_CLASS_IS_ACTIVE' => ''
-    ]));
-
-    if (!empty($navigationsItemsTransformed)) {
-      $pageNavigationTransformed = ThemeCollector::assemblyFileContent($this->CMSCore->theme, 'templates/page/navigationHorizontal.tpl', [
-        'NAVIGATION_LIST' => ThemeCollector::assemblyFileContent($this->CMSCore->theme, 'templates/page/navigationHorizontal/list.tpl', [
-          'NAVIGATION_ITEMS' => implode($navigationsItemsTransformed)
-        ])
-      ]);
-    } else {
-      $pageNavigationTransformed = '';
-    }
-
     $entryTitle = $this->entry->getTitle($localeName);
     $entryTitle = !empty($entryTitle) ? $entryTitle : sprintf('[ TITLE NOT FOUND IN LOCALE %s ]', $localeName);
 
     /** @var string $site_page Содержимое шаблона страницы */
     $this->assembled = ThemeCollector::assemblyFileContent($this->CMSCore->theme, 'templates/page/analytics/entry.tpl', [
-      'PAGE_NAVIGATION' => $pageNavigationTransformed,
       'ADMIN_PANEL_PAGE_NAME' => 'analytics',
       'PAGE_ANALYTICS_ENTRY_TITLE' => sprintf($localeData['PAGE_ANALYTICS_ENTRY_TITLE'], $entryTitle),
       'ENTRY_NAME' => $this->entry->getName()
