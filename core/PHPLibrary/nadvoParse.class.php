ea6305a5c0174cdd6f0311da4eb294a0e4fc150c
Drelagas <drelagas.new@gmail.com>
2025-08-05 16:58:22 +0300

    0.2.4.7-7 воицы - исправление работы с кодировкой в NadvoParse;

--- core/PHPLibrary/nadvoParse.class.php
+++ core/PHPLibrary/nadvoParse.class.php
@@ -3,7 +3,7 @@
 /**
  * CMS GIRVAS (https://www.cms-girvas.ru/)
  * 
- * @link    https://gitflic.ru/project/garbalo/cms-girvas Путь до репозитория системы
+ * @link  https://gitflic.ru/project/garbalo/cms-girvas Путь до репозитория системы
  * @copyright   Copyright (c) 2021 - 2025, Andrey Shestakov & Garbalo (https://www.garbalo.com/)
  * @license   https://gitflic.ru/project/garbalo/cms-girvas/LICENSE.md
  */
@@ -26,211 +26,79 @@ class NadvoParse
 
   public function parse(string $markdown) : string
   {
-    // Нормализация кодировки перед обработкой
-    $markdown = $this->normalizeEncoding($markdown);
+    // Предварительная очистка текста
+    $markdown = $this->preprocessText($markdown);
     
-    $lines = preg_split('/\R/', $markdown);
-    $HTML = '';
+    $lines = explode("\n", $markdown);
+    $html = '';
     $currentParagraph = '';
 
     foreach ($lines as $line) {
-      if (preg_match('/^(#{1,6})\s(.+)/', $line, $matches)) {
-        if ($currentParagraph !== '') {
-          $HTML .= $this->parseParagraph($currentParagraph);
+      $line = trim($line);
+      
+      if (preg_match('/^(#{1,6})\s+(.+)/', $line, $matches)) {
+        if (!empty($currentParagraph)) {
+          $html .= $this->parseParagraph($currentParagraph);
           $currentParagraph = '';
         }
-        $HTML .= $this->parseHeader($line);
-      } elseif (trim($line) === '') {
-        if ($currentParagraph !== '') {
-          $HTML .= $this->parseParagraph($currentParagraph);
+        $html .= $this->parseHeader($line);
+      } elseif (empty($line)) {
+        if (!empty($currentParagraph)) {
+          $html .= $this->parseParagraph($currentParagraph);
           $currentParagraph = '';
         }
       } else {
-        $currentParagraph .= $line . "\n";
+        $currentParagraph .= $line . ' ';
       }
     }
 
-    if ($currentParagraph !== '') {
-      $HTML .= $this->parseParagraph($currentParagraph);
+    if (!empty($currentParagraph)) {
+      $html .= $this->parseParagraph($currentParagraph);
     }
 
-    return $HTML;
+    return $html;
   }
 
-  private function normalizeEncoding(string $text) : string
+  private function preprocessText(string $text) : string
   {
-    if (mb_detect_encoding($text, 'UTF-8', true) === 'UTF-8') {
-      return $text;
-    }
-
-    // Проверяем кодировку
-    $encoding = mb_detect_encoding($text, ['Windows-1251', 'CP866'], true);
+    $text = preg_replace('/[^\x{0000}-\x{FFFF}]/u', '', $text);
     
-    if ($encoding) {
-      $text = mb_convert_encoding($text, 'UTF-8', $encoding);
-    }
-
-    // Удаляем битые символы
-    return preg_replace('/[^\x{0000}-\x{FFFF}]/u', '', $text);
-  }
-
-  private function tokenize(string $markdown) : array
-  {
-    $tokens = [];
-    $offset = 0;
-    $length = mb_strlen($markdown, 'UTF-8');
-
-    while ($offset < $length) {
-      $matched = false;
-      $substr = mb_substr($markdown, $offset, $length - $offset, 'UTF-8');
-
-      foreach (self::PATTERNS as $type => $pattern) {
-        if (preg_match($pattern, $substr, $matches, PREG_OFFSET_CAPTURE)) {
-          $matchText = $matches[0][0];
-          $matchLength = mb_strlen($matchText, 'UTF-8');
-          $matchPos = $offset + $matches[0][1];
-
-          $token = [
-            'type' => $type,
-            'value' => $matchText,
-            'position' => $matchPos,
-          ];
-
-          if ($type === 'header') {
-            $token['level'] = mb_strlen($matches[1][0], 'UTF-8');
-            $token['content'] = $matches[2][0];
-          } elseif (in_array($type, ['bold', 'italic', 'link', 'image'])) {
-            // Для link и image используем первую capture-группу
-            $contentKey = ($type === 'link' || $type === 'image') ? 1 : 0;
-            $token['content'] = $matches[$contentKey + 1][0] ?? $matchText;
-          }
-
-          $tokens[] = $token;
-          $offset += $matchLength;
-          $matched = true;
-          break;
-        }
-      }
-
-      if (!$matched) {
-        $char = mb_substr($markdown, $offset, 1, 'UTF-8');
-        $tokens[] = [
-          'type' => 'text',
-          'value' => $char,
-          'position' => $offset
-        ];
-        $offset++;
-      }
-    }
-
-    return $tokens;
-  }
-
-  private function parseTokens(array $tokens) : array
-  {
-    $AST = [];
-
-    while (!empty($tokens)) {
-      $token = array_shift($tokens);
-
-      switch ($token['type']) {
-        case 'header':
-          $AST[] = [
-            'type' => 'header',
-            'level' => $token['level'],
-            'children' => $this->parseTokens($this->tokenize($token['content'])),
-          ];
-          break;
-        case 'bold':
-        case 'italic':
-          $AST[] = [
-            'type' => $token['type'],
-            'children' => $this->parseTokens($this->tokenize($token['content'])),
-          ];
-          break;
-        case 'link':
-          $AST[] = [
-            'type' => 'link',
-            'url' => $this->sanitizeUrl($matches[2][0] ?? ''),
-            'children' => $this->parseTokens($this->tokenize($token['content'])),
-          ];
-          break;
-        case 'image':
-          $AST[] = [
-            'type' => 'image',
-            'url' => $this->sanitizeUrl($matches[2][0] ?? ''),
-            'alt' => $token['content'],
-          ];
-          break;
-        default:
-          $AST[] = ['type' => 'text', 'value' => $token['value']];
-      }
-    }
-
-    return $AST;
-  }
-
-  private function sanitizeUrl(string $url) : string
-  {
-    $url = trim($url);
-    $url = preg_replace('/[\s<>"\']/', '', $url);
-    
-    if (!preg_match('~^(?:f|ht)tps?://~i', $url)) {
-      $url = 'https://' . $url;
-    }
-
-    return htmlspecialchars($url, ENT_QUOTES, 'UTF-8');
-  }
-
-  private function ASTToHTML(array $AST) : string
-  {
-    $HTML = '';
-
-    foreach ($AST as $node) {
-      switch ($node['type']) {
-        case 'header':
-          $level = min(max($node['level'], 1), 6);
-          $HTML .= '<h' . $level . '>' . $this->ASTToHTML($node['children']) . '</h' . $level . '>';
-          break;
-        case 'bold':
-          $HTML .= '<strong>' . $this->ASTToHTML($node['children']) . '</strong>';
-          break;
-        case 'italic':
-          $HTML .= '<em>' . $this->ASTToHTML($node['children']) . '</em>';
-          break;
-        case 'link':
-          $HTML .= '<a href="' . $node['url'] . '">' . $this->ASTToHTML($node['children']) . '</a>';
-          break;
-        case 'image':
-          $HTML .= '<img src="' . $node['url'] . '" alt="' . htmlspecialchars($node['alt']) . '">';
-          break;
-        default:
-          $HTML .= htmlspecialchars($node['value'], ENT_QUOTES, 'UTF-8');
-      }
-    }
-
-    return $HTML;
+    return str_replace(["\r\n", "\r"], "\n", $text);
   }
 
   private function parseHeader(string $line) : string
   {
-    preg_match('/^(#{1,6})\s(.+)/', $line, $matches);
-    $level = mb_strlen($matches[1], 'UTF-8');
+    preg_match('/^(#{1,6})\s+(.+)/', $line, $matches);
+    $level = strlen($matches[1]);
     $content = $matches[2];
-    $tokens = $this->tokenize($content);
-    $AST = $this->parseTokens($tokens);
-
-    return sprintf('<h%d>%s</h%d>', $level, $this->ASTToHTML($AST), $level);
+    
+    return "<h$level>" . $this->parseInline($content) . "</h$level>";
   }
 
   private function parseParagraph(string $text) : string
   {
-    $text = trim($text);
-    $text = preg_replace('/([^\n])\n([^\n])/', '$1<br>$2', $text);
+    return '<p>' . $this->parseInline($text) . '</p>';
+  }
 
-    $tokens = $this->tokenize($text);
-    $AST = $this->parseTokens($tokens);
+  private function parseInline(string $text) : string
+  {
+    // Обработка ссылок
+    $text = preg_replace_callback(
+      '/\[([^]]+)]\(([^)]+)\)/',
+      function ($matches) {
+        $url = htmlspecialchars(trim($matches[2]), ENT_QUOTES, 'UTF-8');
+        $title = htmlspecialchars($matches[1], ENT_QUOTES, 'UTF-8');
+        return "<a href=\"$url\">$title</a>";
+      },
+      $text
+    );
 
-    return '<p>' . $this->ASTToHTML($AST) . '</p>';
+    // Обработка жирного текста
+    $text = preg_replace('/\*\*([^*]+)\*\*/', '<strong>$1</strong>', $text);
+    
+    // Обработка курсива
+    $text = preg_replace('/\*([^*]+)\*/', '<em>$1</em>', $text);
+
+    return htmlspecialchars_decode($text);
   }
 }
