1c09948be357c30066baa934c0a7432ec529c684
Drelagas <drelagas.new@gmail.com>
2025-08-05 17:23:15 +0300

    0.2.4.7-10 воицы - добавление поддержки таблиц и зачеркнутого текста в NadvoParse;

--- core/PHPLibrary/nadvoParse.class.php
+++ core/PHPLibrary/nadvoParse.class.php
@@ -18,6 +18,7 @@ class NadvoParse
     'italic' => '/\*(.+?)\*|_(.+?)_/s',
     'link' => '/\[([^\[\]]+)\]\(\s*(\S+)\s*\)/',
     'image' => '/!\[(.+?)\]\((.+?)\)/',
+    'table' => '/^(\|.+)+\|$/m',
     'text' => '/[^\*_!\[\]]+/s'
   ];
 
@@ -26,8 +27,68 @@ class NadvoParse
 
   public function parse(string $markdown) : string
   {
+    $markdown = $this->parseTables($markdown);
     $markdown = $this->parseBlocks($markdown);
     return $this->parseInlineElements($markdown);
+  }
+
+  private function parseTables(string $markdown) : string
+  {
+    return preg_replace_callback(
+      '/^([^\n\|]+\|[^\n]+)\n([\-:\| ]+)+\n((?:[^\n]+\|.+\n?)+)/m',
+      function($matches) {
+        $headers = explode('|', trim($matches[1], "| \t\n\r\0\x0B"));
+        $aligns = $this->parseTableAligns(trim($matches[2], "| \t\n\r\0\x0B"));
+        $rows = explode("\n", trim($matches[3]));
+        
+        $html = "<table>\n<thead>\n<tr>\n";
+        
+        // Заголовки таблицы
+        foreach ($headers as $i => $header) {
+          $align = $aligns[$i] ?? '';
+          $html .= "  <th style=\"text-align:$align\">" . trim($header) . "</th>\n";
+        }
+        
+        $html .= "</tr>\n</thead>\n<tbody>\n";
+        
+        // Строки таблицы
+        foreach ($rows as $row) {
+          $cells = explode('|', trim($row, "| \t\n\r\0\x0B"));
+          $html .= "<tr>\n";
+          
+          foreach ($cells as $i => $cell) {
+            $align = $aligns[$i] ?? '';
+            $html .= "  <td style=\"text-align:$align\">" . trim($cell) . "</td>\n";
+          }
+          
+          $html .= "</tr>\n";
+        }
+        
+        return $html . "</tbody>\n</table>";
+      },
+      $markdown
+    );
+  }
+
+  private function parseTableAligns(string $alignRow) : array
+  {
+    $aligns = [];
+    $parts = explode('|', trim($alignRow, "| "));
+    
+    foreach ($parts as $part) {
+      $part = trim($part);
+      if (str_starts_with($part, ':') && str_ends_with($part, ':')) {
+        $aligns[] = 'center';
+      } elseif (str_ends_with($part, ':')) {
+        $aligns[] = 'right';
+      } elseif (str_starts_with($part, ':')) {
+        $aligns[] = 'left';
+      } else {
+        $aligns[] = '';
+      }
+    }
+    
+    return $aligns;
   }
 
   private function parseBlocks(string $markdown) : string
@@ -35,8 +96,22 @@ class NadvoParse
     $lines = explode("\n", $markdown);
     $html = '';
     $currentParagraph = '';
+    $inTable = false;
 
     foreach ($lines as $line) {
+      if (str_starts_with(trim($line), '|')) {
+        if (!$inTable) {
+          if (!empty($currentParagraph)) {
+            $html .= '<p>' . $currentParagraph . '</p>';
+            $currentParagraph = '';
+          }
+          $inTable = true;
+        }
+        continue;
+      }
+
+      $inTable = false;
+
       if (preg_match('/^(#{1,6})\s+(.+)/', $line, $matches)) {
         if (!empty($currentParagraph)) {
           $html .= '<p>' . $currentParagraph . '</p>';
@@ -67,6 +142,8 @@ class NadvoParse
     
     $html = preg_replace('/\*([^*]+)\*/s', '<em>$1</em>', $html);
     $html = preg_replace('/_([^_]+)_/s', '<em>$1</em>', $html);
+    
+    $html = preg_replace('/\~\~(.+?)\~\~/s', '<u>$1</u>', $html);
 
     $html = preg_replace_callback(
       '/!\[(.+?)\]\((.+?)\)/',
