6fa1983cc8fccb78df902092be30897e0b992f66
Drelagas <drelagas.new@gmail.com>
2025-10-07 15:39:32 +0300

    0.2.6.3-9 Воицы - Рефакторинг кода;

--- core/PHPLibrary/entriesSample.class.php
+++ core/PHPLibrary/entriesSample.class.php
@@ -13,6 +13,7 @@ namespace core\PHPLibrary;
 use \core\PHPLibrary\Database\QueryBuilder as DatabaseQueryBuilder;
 use \core\PHPLibrary\Database\DatabaseManagementSystem as CMSDMS;
 use \core\PHPLibrary\Entities\Types\Content as EntityTypeContent;
+use \core\PHPLibrary\SystemCore\Locale as CMSLocale;
 use \PDOException as PDOException;
 
 #[\AllowDynamicProperties]
