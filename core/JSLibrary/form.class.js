46962e1d61bdf2934af13b6c36a44b2fd90d529f
Drelagas <drelagas.new@gmail.com>
2025-10-08 22:39:24 +0300

    0.2.6.4-19 Воицы - Мелкое исправление ошибок;

--- core/JSLibrary/form.class.js
+++ core/JSLibrary/form.class.js
@@ -47,16 +47,18 @@ export class Form {
 
     if (inputsArray.length > 0) {
       inputsArray.forEach((element) => {
+        const elementStyle = element.getComputedStyle();
+
         if (element.hasAttribute('cmsg-password-checker')) {
-          let passwordProgressLevelElement = document.createElement('div');
-          let passwordProgressLevelContainerElement = document.createElement('div');
+          const passwordProgressLevelElement = document.createElement('div');
+          const passwordProgressLevelContainerElement = document.createElement('div');
 
           passwordProgressLevelContainerElement.append(passwordProgressLevelElement);
 
           passwordProgressLevelContainerElement.style.height = '5px';
           passwordProgressLevelContainerElement.style.width = '100%';
           passwordProgressLevelContainerElement.style.position = 'absolute';
-          passwordProgressLevelContainerElement.style.bottom = '-5px';
+          passwordProgressLevelContainerElement.style.top = elementStyle.height + 'px';
 
           passwordProgressLevelElement.style.height = '100%';
           passwordProgressLevelElement.style.width = '25%';
