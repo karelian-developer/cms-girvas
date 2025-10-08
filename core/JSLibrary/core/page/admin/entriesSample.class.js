adf546607b90ed22be15369e4518b03f830ad611
Drelagas <drelagas.new@gmail.com>
2025-10-08 21:15:14 +0300

    0.2.6.4-12 Воицы - Обновление внешнего вида, обработчика страницы редактирования выборки;

--- core/JSLibrary/core/page/admin/entriesSample.class.js
+++ core/JSLibrary/core/page/admin/entriesSample.class.js
@@ -21,12 +21,12 @@ export class PageEntriesSample {
 
   init() {
     let searchParams = new URLParser();
-    let elementForm = document.querySelector('.form_entries-sample');
+    let elementForm = document.querySelector('[data-element="main-form"]');
 
     let locales;
-    let interactiveLocaleChoices = new Interactive('choices');
-    let interactiveCategoriesChoices = new Interactive('choices');
-    let interactiveSortTypeChoices = new Interactive('choices');
+    const interactiveLocaleChoices = new Interactive('choices');
+    const interactiveCategoriesChoices = new Interactive('choices');
+    const interactiveSortTypeChoices = new Interactive('choices');
 
     fetch('/handler/locales', {method: 'GET'}).then((response) => {
       return (response.ok) ? response.json() : Promise.reject(response);
@@ -36,9 +36,9 @@ export class PageEntriesSample {
     }, (rejectionReason) => {
       this.page.showPopupNotification(rejectionReason, 0);
     }).then((localeData) => {
-      let descriptionTextareaElement = document.querySelector('[role="entriesSampleDescription"]');
-      let titleInputElement = document.querySelector('[role="entriesSampleTitle"]');
-      let urlInputElement = document.querySelector('[role="entriesSampleName"]');
+      let urlInputElement = document.querySelector('[data-element="input-name"]');
+      let titleInputElement = document.querySelector('[data-element="input-title"]');
+      let descriptionTextareaElement = document.querySelector('[data-element="input-description"]');
 
       locales.forEach((locale, localeIndex) => {
         let localeTitle = locale.title;
@@ -95,10 +95,10 @@ export class PageEntriesSample {
 
       interactiveLocaleChoices.assembly();
 
-      let interactiveContainerElement = document.querySelector('#E8548530785');
+      let interactiveContainerElement = document.querySelector('[data-element="header-interactive"]');
       interactiveContainerElement.append(interactiveLocaleChoices.target.element);
 
-      let interactiveSortByChoiceMultipleContainerElement = document.querySelector('#TC6474389603');
+      let interactiveSortByChoiceMultipleContainerElement = document.querySelector('[data-element="choice"][data-choice="sort-method"]');
       if (interactiveSortByChoiceMultipleContainerElement != null) {
         let entriesSampleData;
 
@@ -137,7 +137,7 @@ export class PageEntriesSample {
         });
       }
 
-      let interactiveCategoriesChoiceMultipleContainerElement = document.querySelector('#TC6474389602');
+      let interactiveCategoriesChoiceMultipleContainerElement = document.querySelector('[data-element="choice"][data-choice="categories"]');
       if (interactiveCategoriesChoiceMultipleContainerElement != null) {
         let entriesCategories, entriesSampleCategories;
         
@@ -230,15 +230,17 @@ export class PageEntriesSample {
       this.buttons.save.target.setCallback((event) => {
         event.preventDefault();
 
-        elementForm = document.querySelector('.form_entries-sample');
+        elementForm = document.querySelector('[data-element="main-form"]');
 
         let form = new Interactive('form');
         form.target.replaceElement(elementForm);
         
         if (form.target.checkRequiredFields()) {
           let formData = new FormData(elementForm);
-          let fetchLink = (searchParams.getPathPart(3) == null) ? '/handler/entries/sample?localeMessage=' + window.CMSCore.locales.admin.name : '/handler/entries/sample/' + searchParams.getPathPart(3) + '?localeMessage=' + window.CMSCore.locales.admin.name;
-          let fetchMethod = (searchParams.getPathPart(3) == null) ? 'PUT' : 'PATCH';
+          let fetchLink = searchParams.getPathPart(3) === null
+            ? '/handler/entries/sample?localeMessage=' + window.CMSCore.locales.admin.name
+            : '/handler/entries/sample/' + searchParams.getPathPart(3) + '?localeMessage=' + window.CMSCore.locales.admin.name;
+          let fetchMethod = searchParams.getPathPart(3) === null ? 'PUT' : 'PATCH';
 
           let request = new Interactive('request', {
             method: fetchMethod,
@@ -297,7 +299,7 @@ export class PageEntriesSample {
       });
       this.buttons.delete.assembly();
 
-      if (searchParams.getPathPart(3) == null) {
+      if (searchParams.getPathPart(3) === null) {
         this.buttons.delete.target.element.style.display = 'none';
         this.buttons.save.target.element.style.display = 'flex';
       } else {
@@ -305,7 +307,7 @@ export class PageEntriesSample {
         this.buttons.save.target.element.style.display = 'flex';
       }
 
-      let interactiveContainer = document.querySelector('#SYSTEM_E3724126170');
+      let interactiveContainer = document.querySelector('[data-element="panel"]');
       interactiveContainer.append(this.buttons.delete.target.element);
       interactiveContainer.append(this.buttons.save.target.element);
     }, (rejectionReason) => {
