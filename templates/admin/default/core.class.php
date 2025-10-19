566ab80261b929c0c4c248f7a30d41596c3e1bee
Drelagas <drelagas.new@gmail.com>
2025-10-19 03:39:36 +0300

    0.2.7.2-3 Воицы - Оптимизация кода;

--- templates/admin/default/core.class.php
+++ templates/admin/default/core.class.php
@@ -12,7 +12,7 @@ namespace templates\admin\default;
 
 use \core\PHPLibrary\Database\QueryBuilder as DatabaseQueryBuilder;
 use \core\PHPLibrary\Template as Theme;
-use \core\PHPLibrary\InterfaceTheme as ThemeInterface;
+use \core\PHPLibrary\ThemeInterface as ThemeInterface;
 use \core\PHPLibrary\Template\Collector as ThemeCollector;
 use \core\PHPLibrary\Template\InterfaceCore as ThemeInterfaceCore;
 use \core\PHPLibrary\Entry as Entry;
