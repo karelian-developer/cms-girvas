8b31eef8ac58cb5295d177e374f1025441adb8b9
Drelagas <drelagas.new@gmail.com>
2025-11-01 22:01:34 +0300

    0.2.7.4-7 Воицы - реализация клиентской обработки страниц с формами в административной панели;

---
+++ core/JSLibrary/core/page/admin/form.class.js
@@ -0,0 +1,271 @@
+/**
+ * CMS GIRVAS (https://www.cms-girvas.ru/)
+ * 
+ * @link        https://gitflic.ru/project/garbalo/cms-girvas Путь до репозитория системы
+ * @copyright   Copyright (c) 2022 - 2024, Andrey Shestakov & Garbalo (https://www.garbalo.com/)
+ * @license     https://gitflic.ru/project/garbalo/cms-girvas/LICENSE.md
+ */
+
+'use strict';
+
+import {Interactive} from "../../../interactive.class.js";
+import {URLParser} from "../../../urlParser.class.js";
+import {Utils} from "../../../utils.class.js";
+
+export class PageEntriesSample {
+  constructor(page, params = {}) {
+    this.page = page;
+    
+    this.buttons = {save: null, delete: null};
+  }
+
+  init() {
+    let searchParams = new URLParser();
+    let elementForm = document.querySelector('[data-element="main-form"]');
+
+    let locales;
+    const interactiveLocaleChoices = new Interactive('choices');
+    const interactiveMethodChoices = new Interactive('choices');
+
+    fetch('/handler/locales', {method: 'GET'}).then((response) => {
+      return (response.ok) ? response.json() : Promise.reject(response);
+    }).then((data) => {
+      locales = data.outputData.locales;
+      return window.CMSCore.locales.admin.getData();
+    }, (rejectionReason) => {
+      this.page.showPopupNotification(rejectionReason, 0);
+    }).then((localeData) => {
+      let urlInputElement = document.querySelector('[data-element="input-name"]');
+      let titleInputElement = document.querySelector('[data-element="input-title"]');
+      let descriptionTextareaElement = document.querySelector('[data-element="input-description"]');
+
+      locales.forEach((locale, localeIndex) => {
+        let localeTitle = locale.title;
+        let localeIconURL = locale.iconURL;
+        let localeName = locale.name;
+        let localeISO639_2 = locale.iso639_2;
+
+        let localeIconImageElement = document.createElement('img');
+        localeIconImageElement.setAttribute('src', localeIconURL);
+        localeIconImageElement.setAttribute('alt', localeTitle);
+
+        let localeLabelElement = document.createElement('span');
+        localeLabelElement.innerText = localeTitle;
+
+        let localeTemplate = document.createElement('template');
+        localeTemplate.innerHTML += localeIconImageElement.outerHTML;
+        localeTemplate.innerHTML += localeLabelElement.outerHTML;
+
+        interactiveLocaleChoices.target.addItem(localeTemplate.innerHTML, localeName);
+      });
+
+      locales.forEach((locale, localeIndex) => {
+        if (locale.name === window.CMSCore.locales.admin.name) {
+          interactiveLocaleChoices.target.setItemSelectedIndex(localeIndex);
+        }
+
+        if (locale.name === window.CMSCore.locales.admin.name) {
+          descriptionTextareaElement.setAttribute('name', 'form_description_' + locale.iso639_2);
+          titleInputElement.setAttribute('name', 'form_title_' + locale.iso639_2);
+
+          if (searchParams.getPathPart(3) != null) {
+            let request = new Interactive('request', {
+              method: 'GET',
+              url: `/handler/form/${searchParams.getPathPart(3)}?locale=${window.CMSCore.locales.admin.name}`
+            });
+    
+            request.target.showingNotification = false;
+            
+            let formConstrData;
+            request.target.send().then((data) => {
+              if (data.statusCode == 1 && data.outputData.hasOwnProperty('form')) {
+                formConstrData = data.outputData.form;
+
+                descriptionTextareaElement.value = data.outputData.form.description;
+                titleInputElement.value = data.outputData.form.title;
+              }
+
+            }, (rejectionReason) => {
+              this.page.showPopupNotification(rejectionReason, 0);
+            });
+          }
+        }
+      });
+
+      interactiveLocaleChoices.assembly();
+
+      let interactiveContainerElement = document.querySelector('[data-element="header-interactive"]');
+      interactiveContainerElement.append(interactiveLocaleChoices.target.element);
+
+      let interactiveMethodContainerElement = document.querySelector('[data-element="choice"][data-choice="method"]');
+      if (interactiveMethodContainerElement != null) {
+        let formConstrData;
+
+        fetch(`/handler/form/${searchParams.getPathPart(3)}?locale=${window.CMSCore.locales.admin.name}`, {method: 'GET'}).then((response) => {
+          return (response.ok) ? response.json() : Promise.reject(response);
+        }).then((data) => {
+          formConstrData = data.outputData.form;
+
+          return fetch(`/handler/form/methods?locale=${window.CMSCore.locales.admin.name}`, {method: 'GET'});
+        }, (rejectionReason) => {
+          this.page.showPopupNotification(rejectionReason, 0);
+        }).then((response) => {
+          return (response.ok) ? response.json() : Promise.reject(response);
+        }).then((data) => {
+          if (data.outputData.hasOwnProperty('methods')) {
+            if (data.outputData.methods.length > 0) {
+              data.outputData.methods.forEach((method, methodIndex) => {
+                let methodTitle = method.name.toUpperCase();
+                interactiveMethodChoices.target.addItem(methodTitle, method.id);
+                
+                console.log(formConstrData);
+                if (formConstrData !== undefined) {
+                  if (formConstrData.sortTypeID == method.id) {
+                    interactiveMethodChoices.target.setItemSelectedIndex(methodIndex);
+                  }
+                }
+              });
+
+              interactiveMethodChoices.target.setName('form_method_id');
+              interactiveMethodChoices.assembly();
+            }
+          }
+          
+          interactiveMethodContainerElement.append(interactiveMethodChoices.target.element);
+        });
+      }
+
+      urlInputElement.addEventListener('input', (event) => {
+        /** @var {String} */
+        let inputValue = event.target.value;
+
+        /** @var {Utils} */
+        let utils = new Utils();
+        /** @var {UString} */
+        let uString = utils.createString(inputValue);
+        uString.source = uString.translitToEN(true);
+        uString.source = uString.source.toLowerCase();
+        uString.source = uString.source.replace(/[^a-z0-9\-]/, '');
+
+        event.target.value = uString.source;
+      });
+      
+      let interactiveChoicesSelectElement = interactiveContainerElement.querySelector('select');
+      interactiveChoicesSelectElement.addEventListener('change', (event) => {
+        let formTitleInputElement = document.querySelector('[data-element="input-title"]');
+        let formDescriptionTextareaElement = document.querySelector('[data-element="input-description"]');
+        
+        locales.forEach((locale, localeIndex) => {
+          if (locale.name == event.target.value) {
+            formTitleInputElement.setAttribute('name', 'form_title_' + locale.iso639_2);
+            formDescriptionTextareaElement.setAttribute('name', 'form_description_' + locale.iso639_2);
+
+            if (searchParams.getPathPart(3) != null) {
+              let request = new Interactive('request', {
+                method: 'GET',
+                url: '/handler/form/' + searchParams.getPathPart(3) + '?locale=' + locale.name + '&localeMessage=' + window.CMSCore.locales.admin.name
+              });
+      
+              request.target.showingNotification = false;
+      
+              request.target.send().then((data) => {
+                if (data.statusCode === 1 && data.outputData.hasOwnProperty('form')) {
+                  formDescriptionTextareaElement.value = data.outputData.form.description;
+                  formTitleInputElement.value = data.outputData.form.title;
+                }
+              });
+            }
+          }
+        });
+      });
+
+      this.buttons.save = new Interactive('button');
+      this.buttons.save.target.setLabel(localeData.BUTTON_SAVE_LABEL);
+      this.buttons.save.target.setCallback((event) => {
+        event.preventDefault();
+
+        elementForm = document.querySelector('[data-element="main-form"]');
+
+        let form = new Interactive('form');
+        form.target.replaceElement(elementForm);
+        
+        if (form.target.checkRequiredFields()) {
+          let formData = new FormData(elementForm);
+          let fetchLink = searchParams.getPathPart(3) === null
+            ? '/handler/form?localeMessage=' + window.CMSCore.locales.admin.name
+            : '/handler/form/' + searchParams.getPathPart(3) + '?localeMessage=' + window.CMSCore.locales.admin.name;
+          let fetchMethod = searchParams.getPathPart(3) === null ? 'PUT' : 'PATCH';
+
+          let request = new Interactive('request', {
+            method: fetchMethod,
+            url: fetchLink
+          });
+  
+          request.target.data = formData;
+  
+          request.target.send().then((data) => {
+            if (data.statusCode == 1 && searchParams.getPathPart(3) == null) {
+              let formConstrData = data.outputData.form;
+              window.location.href = '/admin/form/' + formConstrData.id;
+            }
+          });
+        } else {
+          this.page.showPopupNotification(localeData.FORM_REQUIRED_FIELDS_IS_EMPTY, 0);
+        }
+      });
+      this.buttons.save.assembly();
+
+      this.buttons.delete = new Interactive('button');
+      this.buttons.delete.target.setLabel(localeData.BUTTON_DELETE_LABEL);
+      this.buttons.delete.target.setCallback((event) => {
+        event.preventDefault();
+
+        let interactiveModal = new Interactive('modal', {
+          title: localeData.MODAL_ENTRIES_CATEGORY_DELETE_TITLE,
+          content: localeData.MODAL_ENTRIES_CATEGORY_DELETE_DESCRIPTION
+        });
+        
+        interactiveModal.target.addButton(localeData.BUTTON_DELETE_LABEL, () => {
+          let formData = new FormData();
+          formData.append('form_id', searchParams.getPathPart(3));
+
+          let request = new Interactive('request', {
+            method: 'DELETE',
+            url: '/handler/form/' + searchParams.getPathPart(3) + '?localeMessage=' + window.CMSCore.locales.admin.name
+          });
+
+          request.target.data = formData;
+
+          request.target.send().then((data) => {
+            if (data.statusCode == 1) {
+              window.location.href = '/admin/forms';
+            }
+          });
+        });
+
+        interactiveModal.target.addButton(localeData.BUTTON_CANCEL_LABEL, () => {
+          interactiveModal.target.close();
+        });
+
+        interactiveModal.assembly();
+        document.body.appendChild(interactiveModal.target.element);
+        interactiveModal.target.show();
+      });
+      this.buttons.delete.assembly();
+
+      if (searchParams.getPathPart(3) === null) {
+        this.buttons.delete.target.element.style.display = 'none';
+        this.buttons.save.target.element.style.display = 'flex';
+      } else {
+        this.buttons.delete.target.element.style.display = 'flex';
+        this.buttons.save.target.element.style.display = 'flex';
+      }
+
+      let interactiveContainer = document.querySelector('[data-element="panel"]');
+      interactiveContainer.append(this.buttons.delete.target.element);
+      interactiveContainer.append(this.buttons.save.target.element);
+    }, (rejectionReason) => {
+      this.page.showPopupNotification(rejectionReason, 0);
+    });
+  }
+}
---
+++ core/JSLibrary/core/page/admin/forms.class.js
@@ -0,0 +1,91 @@
+/**
+ * CMS GIRVAS (https://www.cms-girvas.ru/)
+ * 
+ * @link        https://gitflic.ru/project/garbalo/cms-girvas Путь до репозитория системы
+ * @copyright   Copyright (c) 2022 - 2024, Andrey Shestakov & Garbalo (https://www.garbalo.com/)
+ * @license     https://gitflic.ru/project/garbalo/cms-girvas/LICENSE.md
+ */
+
+'use strict';
+
+import {Interactive} from "../../../interactive.class.js";
+
+export class PageFeeds {
+  constructor(page, params = {}) {
+    this.page = page;
+
+    this.buttons = {save: null, delete: null};
+  }
+
+  init() {
+    let locales;
+
+    fetch('/handler/locales', {method: 'GET'}).then((response) => {
+      return (response.ok) ? response.json() : Promise.reject(response);
+    }).then((data) => {
+      locales = data.outputData.locales;
+      return window.CMSCore.locales.admin.getData();
+    }, (rejectionReason) => {
+      this.page.showPopupNotification(rejectionReason, 0);
+    }).then((localeData) => {
+
+      const interactiveCreatePageButton = new Interactive('button');
+      interactiveCreatePageButton.target.setLabel(localeData.BUTTON_NEW_FORM_LABEL);
+      interactiveCreatePageButton.target.setCallback(() => {
+        window.location.href = `./form`;
+      });
+      interactiveCreatePageButton.assembly();
+    
+      const interactiveContainerElement = document.querySelector('#E8548530785');
+      interactiveContainerElement.append(interactiveCreatePageButton.target.element);
+
+      const tableItems = document.querySelectorAll('[data-element="form"]');
+      for (let tableItem of tableItems) {
+        const formID = tableItem.getAttribute('data-id');
+        const panelElement = tableItem.querySelector('[data-element="panel"]');
+        const panelEventElements = panelElement.querySelectorAll('[data-event]');
+        
+        for (let eventElement of panelEventElements) {
+          eventElement.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            if (eventElement.getAttribute('data-event') === 'remove') {
+              const interactiveModal = new Interactive('modal', {
+                title: localeData.MODAL_FORM_DELETE_TITLE,
+                content: localeData.MODAL_FORM_DELETE_DESCRIPTION
+              });
+              
+              interactiveModal.target.addButton(localeData.BUTTON_DELETE_LABEL, () => {
+                let formData = new FormData();
+                formData.append('web_channel_id', formID);
+
+                let request = new Interactive('request', {
+                  method: 'DELETE',
+                  url: '/handler/form/' + formID + '?localeMessage=' + window.CMSCore.locales.admin.name
+                });
+      
+                request.target.data = formData;
+      
+                request.target.send().then((data) => {
+                  if (data.statusCode === 1) {
+                    window.location.href = '/admin/forms';
+                  }
+                });
+              });
+
+              interactiveModal.target.addButton(localeData.BUTTON_CANCEL_LABEL, () => {
+                interactiveModal.target.close();
+              });
+
+              interactiveModal.assembly();
+              document.body.appendChild(interactiveModal.target.element);
+              interactiveModal.target.show();
+            }
+          });
+        }
+      }
+    }, (rejectionReason) => {
+      this.page.showPopupNotification(rejectionReason, 0);
+    });
+  }
+}
