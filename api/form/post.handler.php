92423c414e27361674fe7344551e25618f6158e9
Drelagas <drelagas.new@gmail.com>
2025-11-03 21:24:53 +0300

    0.2.7.4-17 Воицы - реализация сохранения и получения данных с форм, созданных через административной панель; - обновлены локализации: русский и английский;

--- api/form/post.handler.php
+++ api/form/post.handler.php
@@ -25,30 +25,28 @@ if (!defined('IS_NOT_HACKED')) {
 
 use \core\PHPLibrary\Form as Form;
 
-if (is_numeric($CMSCore->urlp->getPath(2))) {
-  $formID = (int) $CMSCore->urlp->getPath(2);
+$formName = $CMSCore->urlp->getPath(2);
 
-  if (Form::existsByID($CMSCore, $formID)) {
-    $form = new Form($CMSCore, $formID);
-    $form->initData(['name', 'metadata', 'elements']);
-    $formLocale = $CMSCore->urlp->getParam('locale') ?? $CMSCore->configurator->getDatabaseEntryValue('base_locale');
+if (Form::existsByName($CMSCore, $formName)) {
+  $form = Form::getByName($CMSCore, $formName);
+  $form->initData(['name', 'metadata', 'elements']);
+  $formLocale = $CMSCore->urlp->getParam('locale') ?? $CMSCore->configurator->getDatabaseEntryValue('base_locale');
 
-    $formName = $form->getName();
-    $formData = [];
+  $formName = $form->getName();
+  $formData = [];
 
-    foreach($_POST as $POSTDataKey => $POSTData) {
-      if (preg_match('/^' . $formName . '_([a-z0-9_]+)$/', $POSTDataKey, $matches, PREG_OFFSET_CAPTURE)) {
-        $formFieldName = lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $matches[1][0]))));
-        $formData[$formFieldName] = $POSTData;
-      }
-    };
+  foreach($_POST as $POSTDataKey => $POSTData) {
+    if (preg_match('/^' . $formName . '_([a-z0-9_]+)$/', $POSTDataKey, $matches, PREG_OFFSET_CAPTURE)) {
+      $formFieldName = lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $matches[1][0]))));
+      $formData[$formFieldName] = $POSTData;
+    }
+  };
 
-    $form->saveData($formData);
+  $form->saveData($formData);
 
-    $handlerMessage = $handlerMessage ?? $CMSCore->locale->getSingleValueByKey('API_POST_DATA_SUCCESS');
-    $handlerStatusCode = $handlerStatusCode ?? 1;
-  } else {
-    $handlerMessage = $handlerMessage ?? 'API ERROR: ' . $CMSCore->locale->getSingleValueByKey('API_FORM_ERROR_NOT_FOUND');
-    $handlerStatusCode = $handlerStatusCode ?? 0;
-  }
+  $handlerMessage = $handlerMessage ?? $CMSCore->locale->getSingleValueByKey('API_POST_DATA_SUCCESS');
+  $handlerStatusCode = $handlerStatusCode ?? 1;
+} else {
+  $handlerMessage = $handlerMessage ?? 'API ERROR: ' . $CMSCore->locale->getSingleValueByKey('API_FORM_ERROR_NOT_FOUND');
+  $handlerStatusCode = $handlerStatusCode ?? 0;
 }
