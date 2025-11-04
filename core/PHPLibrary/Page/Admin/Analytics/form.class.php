1f92d5eda79584b32c444beb329dc909f7bd7b0c
Drelagas <drelagas.new@gmail.com>
2025-11-04 22:10:40 +0300

    0.2.7.5-5 Воицы - исправление мелких ошибок

--- core/PHPLibrary/Page/Admin/Analytics/form.class.php
+++ core/PHPLibrary/Page/Admin/Analytics/form.class.php
@@ -107,6 +107,7 @@ class PageForm implements InterfacePage
       foreach ($formsElements as $elementIndex => $elementData) {
         $elementName = $elementData['name'];
         $data['data'] = json_encode($data['data'], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        error_log(print_r($data['data'], true));
 
         if (isset($data['data'][$elementName])) {
           $elementTitle = isset($elementData['texts'][$localeName]['title'])
