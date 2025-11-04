2da795aaf4602f14cc1e243aae887c8ef5dc4ef1
Drelagas <drelagas.new@gmail.com>
2025-11-04 21:52:03 +0300

    0.2.7.5-5 Воицы - исправление мелких ошибок

--- core/PHPLibrary/Page/Admin/Analytics/form.class.php
+++ core/PHPLibrary/Page/Admin/Analytics/form.class.php
@@ -95,7 +95,7 @@ class PageForm implements InterfacePage
     $formsElements = $this->form->getElements();
     $formsData = $this->form->getData();
     $formsDatas = array_slice($formsData, $paginationItemCurrent * $paginationItemsOnPage, $paginationItemsOnPage);
-
+    error_log(print_r($formsDatas, true));
     $pagination = new Pagination($this->CMSCore, count($formsData), $paginationItemsOnPage, $paginationItemCurrent);
     $pagination->assembly();
 
