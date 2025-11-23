394c47fd81e34f1c9b7b6257f4d7b9ce8a0677b5
Drelagas <drelagas.new@gmail.com>
2025-11-23 18:57:40 +0300

    0.2.7.10 воицы - внедрение CSRF-токена для защиты веб-сайтов от атак Cross-Site Request Forgery (CSRF);

--- core/JSLibrary/interactive/request.class.js
+++ core/JSLibrary/interactive/request.class.js
@@ -32,6 +32,7 @@ export class Request {
 
     if (window.CMSCore !== undefined) {
       if (window.CMSCore.client.CSRFToken !== '') {
+        console.log(window.CMSCore.client.CSRFToken);
         this.headers['X-CSRF-Token'] = window.CMSCore.client.CSRFToken;
       }
     }
