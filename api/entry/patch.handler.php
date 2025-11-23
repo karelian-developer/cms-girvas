cf37ec2c37043c5149f64ee60253ed678963376b
Drelagas <drelagas.new@gmail.com>
2025-11-23 20:23:59 +0300

    0.2.7.11 воицы - исправление экранирования данных при записи и markdown-парсера;

--- api/entry/patch.handler.php
+++ api/entry/patch.handler.php
@@ -159,42 +159,36 @@ if ($CMSCore->client->isLogged(2)) {
 
                 if (array_key_exists($inputTitleName, $_PATCH)) {
                   $inputValue = $_PATCH[$inputTitleName];
-                  $inputValue = str_replace('"', '\"', $inputValue);
       
                   $entryData['texts'][$CMSLocaleName]['title'] = $inputValue;
                 }
 
                 if (array_key_exists($inputSEOTitleName, $_PATCH)) {
                   $inputValue = $_PATCH[$inputSEOTitleName];
-                  $inputValue = str_replace('"', '\"', $inputValue);
       
                   $entryData['texts'][$CMSLocaleName]['SEOTitle'] = $inputValue;
                 }
     
                 if (array_key_exists($textareaDescriptionName, $_PATCH)) {
                   $textareaValue = $_PATCH[$textareaDescriptionName];
-                  $textareaValue = str_replace('"', '\"', $textareaValue);
       
                   $entryData['texts'][$CMSLocaleName]['description'] = $textareaValue;
                 }
     
                 if (array_key_exists($textareaSEODescriptionName, $_PATCH)) {
                   $textareaValue = $_PATCH[$textareaSEODescriptionName];
-                  $textareaValue = str_replace('"', '\"', $textareaValue);
       
                   $entryData['texts'][$CMSLocaleName]['SEODescription'] = $textareaValue;
                 }
                 
                 if (array_key_exists($textareaContentName, $_PATCH)) {
                   $textareaValue = $_PATCH[$textareaContentName];
-                  $textareaValue = str_replace('"', '\"', $textareaValue);
       
                   $entryData['texts'][$CMSLocaleName]['content'] = $textareaValue;
                 }
     
                 if (array_key_exists($textareaKeywordsName, $_PATCH)) {
                   $textareaValue = $_PATCH[$textareaKeywordsName];
-                  $textareaValue = str_replace('"', '\"', $textareaValue);
     
                   $entryData['texts'][$CMSLocaleName]['keywords'] = preg_split('/\h*[\,]+\h*/', $textareaValue, -1, PREG_SPLIT_NO_EMPTY);
                 }
