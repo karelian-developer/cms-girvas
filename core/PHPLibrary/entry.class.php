346a58f2658b365c0ce379148b034eb92d58d1d8
Drelagas <drelagas.new@gmail.com>
2025-11-23 20:39:01 +0300

    0.2.7.11 воицы - исправление экранирования данных при записи и markdown-парсера;

--- core/PHPLibrary/entry.class.php
+++ core/PHPLibrary/entry.class.php
@@ -990,8 +990,8 @@ class Entry implements EntityTypeContent
       }
 
       foreach ($data[$columnName] as $name => $value) {
-        $valueJSON = json_encode($value, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-        
+        $valueJSON = json_encode($value, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_HEX_QUOT | JSON_HEX_TAG);
+
         $fieldsJSON[] = match ($queryBuilder->DMS) {
           CMSDMS::MySQL => sprintf("\"%s\": %s", $name, $valueJSON),
           CMSDMS::PostgreSQL => sprintf("'{\"%s\": %s}'::jsonb", $name, $valueJSON)
