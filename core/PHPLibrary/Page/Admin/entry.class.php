93f123f85503259fcb566cf4aa38f6ccb4e85157
Drelagas <drelagas.new@gmail.com>
2025-11-27 22:30:02 +0300

    0.2.7.16 воицы - исправление мелких ошибок;

--- core/PHPLibrary/Page/Admin/entry.class.php
+++ core/PHPLibrary/Page/Admin/entry.class.php
@@ -306,16 +306,14 @@ class PageEntry implements InterfacePage
       );
     }
 
+    $templatesAssembled['ADMIN_PANEL_PAGE_NAME'] = 'entry';
+    $templatesAssembled['ENTRY_EDITOR'] = ThemeCollector::assemblyFileContent(
+      $this->CMSCore->theme, 'templates/page/entry/editor.tpl', []
+    );
+
     /** @var string $site_page Содержимое шаблона страницы */
     $this->assembled = ThemeCollector::assemblyFileContent(
       $this->CMSCore->theme, 'templates/page/entry.tpl',
-      [
-        'ADMIN_PANEL_PAGE_NAME' => 'entry',
-        'ENTRY_EDITOR' => ThemeCollector::assemblyFileContent(
-          $this->CMSCore->theme, 'templates/page/entry/editor.tpl',
-          []
-        )
-      ],
       $templatesAssembled
     );
   }
