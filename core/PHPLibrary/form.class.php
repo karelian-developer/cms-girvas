b95b89f2928b32f551e568bbed28daf7663c4fcc
Drelagas <drelagas.new@gmail.com>
2025-12-09 18:46:05 +0300

    0.2.8 Воицы - Переработка страницы с отчетами в административной панели; - Внедрение механизма добавления значений для шаблонных переменных только по необходимости; - Внедрение анти-брутфорс системы; - Реализация авторесайзинга загружаемых изображений-превью для записей и статических страниц; - Добавление глобальной шаблонной переменной cms_domain_link; - Реализация автогенерации opengraph-данных для страниц сайта; - Исправление экранирования данных при записи и markdown-парсера; - Внедрение csrf-токена для защиты веб-сайтов от атак cross-ite request forgery (csrf); - Добавление оптимизированных вариантов шрифта roboto в административную панель и первичную тему сайта: woff, woff2; - Добавление подраздела <формы> в раздел <аналитика> в административной панели для аналитики результатов сбора информации через индивидуальные формы; - Исправлена ошибка, при которой запись отображалась даже в том случае, если время публикации еще не наступило; - Исправлена ошибка, при которой не подгружались данные по seo-полям на странице редактирования категорий записей; - Реализация классов индивидуальных форм, создаваемых через административную панель с возможностью вывода в теме сайта через шаблонную переменную; - Объединение разделов административнйо панели: записи и статические страницы в единый - контент; - Добавление глобальных переменных в сборщик выборок; - Реализация сборщика шаблонов модуля; - Обновление локализаций: русская и английская; - Исправление мелких ошибок; - Рефакторинг кода;

--- core/PHPLibrary/form.class.php
+++ core/PHPLibrary/form.class.php
@@ -611,6 +611,7 @@ class Form implements EntityTypeContent
     $queryBuilder = new DatabaseQueryBuilder($CMSCore, $CMSConfigDatabase['dms']);
     $queryBuilder->setStatementInsert();
     $queryBuilder->statement->setTable('forms');
+    $queryBuilder->statement->addColumn('id');
     $queryBuilder->statement->addColumn('name');
     $queryBuilder->statement->addColumn('texts');
     $queryBuilder->statement->addColumn('metadata');
