7a2ae8b1a66ebca99044332d2ccb553e2ead4de2
Drelagas <drelagas.new@gmail.com>
2025-12-28 17:22:55 +0300

    init

--- api/form/post.handler.php
+++ api/form/post.handler.php
@@ -100,7 +100,7 @@ if (Form::existsByName($CMSCore, $formName)) {
         $CMSTelegramNotifierMessage .= implode("\n", $formDataFormated) . "\n\n";
         $CMSTelegramNotifierMessage .= "*Дата отправления:* " . $formSendedDatetime . "\n";
         $CMSTelegramNotifierMessage .= "*IP-адрес отправителя:* " . $formSendedAuthorIP . "\n\n";
-        $CMSTelegramNotifierMessage .= "_Отчет *автоматически* сгенерирован системой отчетности *" . $CMSCore::CMS_TITLE . " " . $CMSCore::CMS_VERSION . "*_. Если возникли проблемы с формированием данных, то необходимо обратиться в техническую поддержку компании-производителя «Карельский разработчик»: support@karelian-developer.ru.";
+        $CMSTelegramNotifierMessage .= "_Отчет автоматически сгенерирован системой отчетности " . $CMSCore::CMS_TITLE . " " . $CMSCore::CMS_VERSION . ". Если возникли проблемы с формированием данных, то необходимо обратиться в техническую поддержку компании-производителя «Карельский разработчик»: support@karelian-developer.ru._";
 
         $CMSTelegramNotifier->setMessage($CMSTelegramNotifierMessage);
 
