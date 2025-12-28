3e06625e0eb68092d4a5c720f81994212e3ab4d2
Drelagas <drelagas.new@gmail.com>
2025-12-28 22:22:56 +0300

    0.2.8.1 воицы - исправление мелких ошибок;

--- core/PHPLibrary/SystemCore/Notifier/telegram.class.php
+++ core/PHPLibrary/SystemCore/Notifier/telegram.class.php
@@ -85,10 +85,10 @@ class Telegram extends CMSNotifier
 
   public function setMessage(string $text) : void
   {
-    $chars = ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '!'];
+    $chars = ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '=', '|', '{', '}', '!'];
     
     foreach ($chars as $char) {
-      $text = preg_replace('/(?<!\\\\)' . preg_quote($char, '/') . '/', '\\\\' . $char, $text);
+      $text = str_replace($char, '\\' . $char, $text);
     }
     
     $this->message = $text;
