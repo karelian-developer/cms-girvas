7ca35c04d5e639e6fefffc729ef2d03898f4ac63
Drelagas <drelagas.new@gmail.com>
2026-01-20 20:13:50 +0300

    0.2.9 Воицы - Исправление ошибки инициализации страниц в административной панели;

--- templates/admin/default/core.class.php
+++ templates/admin/default/core.class.php
@@ -277,8 +277,7 @@ final class Core implements ThemeInterfaceCore
   public function assemblyMain(array $themeReplaces = []) : string
   {
     $parsedURL = parse_url($_SERVER['REQUEST_URI']);
-    echo $parsedURL['path'];
-    $this->theme->CMSCore->initPage($parsedURL['path']);
+    $this->theme->CMSCore->initPage(ltrim($parsedURL['path'], '/'));
     
     $sitePage = $this->theme->CMSCore->page;
     $sitePage->assembly();
