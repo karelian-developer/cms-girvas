7e8cebce4bc2afcd413fd70adc182321c66e9a24
Drelagas <drelagas.new@gmail.com>
2026-01-29 13:13:48 +0300

    0.2.9 Воицы - Внедрение механизма интерактивной загрузки векторных иконок;

--- core/JSLibrary/core/page/admin/media.class.js
+++ core/JSLibrary/core/page/admin/media.class.js
@@ -19,14 +19,9 @@ import {Interactive} from "../../../interactive.class.js";
 import {URLParser} from "../../../urlParser.class.js";
 
 export class PageMedia {
-  static buttonIcons = {
-    trash: '<svg version="1.1" class="button__icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 64 64" style="enable-background:new 0 0 64 64;" xml:space="preserve"><rect x="13.9" y="16.1" width="37.3" height="47.9"></rect><path d="M41.4,10l1-7.3L24.7,0.3l-1,7.3L10.2,5.7l-1,7.3l44.6,6.2l1-7.3L41.4,10z M25.6,7.8l0.7-5l13.8,1.9l-0.7,5L25.6,7.8z"></path></svg>',
-    link: '<svg version="1.1" class="button__icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 64 64" style="enable-background:new 0 0 64 64;" xml:space="preserve"><path d="M30.7,29l4.8,4.8c1.3,1.3,1.3,3.5,0,4.9L18.3,55.9c-1.3,1.3-3.5,1.3-4.9,0l-5.3-5.3c-1.3-1.3-1.3-3.5,0-4.9l14-14L20.6,27 L4.9,42.6c-3.1,3.1-3.1,8.1,0,11.1l5.3,5.3c3.1,3.1,8.1,3.1,11.1,0l17.3-17.3c3.1-3.1,3.1-8.1,0-11.1l-5-5L30.7,29z"></path><path d="M33.4,35.1l-4.9-4.9c-1.3-1.3-1.3-3.5,0-4.9L45.7,8.1c1.3-1.3,3.5-1.3,4.9,0l5.3,5.3c1.3,1.3,1.3,3.5,0,4.9L42.8,31.4 l1.4,4.8l14.9-14.9c3.1-3.1,3.1-8.1,0-11.1l-5.3-5.3c-3.1-3.1-8.1-3.1-11.1,0L25.4,22.2c-3.1,3.1-3.1,8.1,0,11.1l5.3,5.3 c0.3,0.3,0.6,0.5,0.9,0.8L33.4,35.1z"></path></svg>',
-    open: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" class="button__icon"><g id="Слой_2" data-name="Слой 2"><rect y="26.42" width="18.42" height="11.17"/><rect x="26.42" width="11.17" height="18.42"/><rect x="45.58" y="26.42" width="18.42" height="11.17"/><rect x="26.42" y="45.58" width="11.17" height="18.42"/></g></svg>'
-  };
-
   constructor(page, params = {}) {
     this.page = page;
+    this.icons = {};
 
     this.buttons = {upload: null};
     this.localeData = null;
@@ -44,7 +39,7 @@ export class PageMedia {
     buttons.link = new Interactive('button');
     buttons.open = new Interactive('button');
 
-    buttons.delete.target.setLabel(PageMedia.buttonIcons.trash);
+    buttons.delete.target.setLabel(this.icons.trash);
     buttons.delete.target.setCallback((event) => {
       event.preventDefault();
       
@@ -81,7 +76,7 @@ export class PageMedia {
       interactiveModal.target.show();
     });
 
-    buttons.link.target.setLabel(PageMedia.buttonIcons.link);
+    buttons.link.target.setLabel(this.icons.link);
     buttons.link.target.setCallback((event) => {
       event.preventDefault();
       navigator.clipboard.writeText(fileURL);
@@ -90,7 +85,7 @@ export class PageMedia {
     });
 
     buttons.open = new Interactive('button');
-    buttons.open.target.setLabel(PageMedia.buttonIcons.open);
+    buttons.open.target.setLabel(this.icons.target);
     buttons.open.target.setCallback((event) => {
       event.preventDefault();
 
@@ -192,8 +187,6 @@ export class PageMedia {
     if (mediaUploaderInput != null) {
       mediaUploaderInput.setAttribute('accept', 'image/png, image/jpeg, image/gif, image/webp, image/avif');
 
-      this.initUploaderInput(mediaUploaderInput);
-
       fetch('/handler/locales', {method: 'GET'}).then((response) => {
         return (response.ok) ? response.json() : Promise.reject(response);
       }).then((data) => {
@@ -204,22 +197,31 @@ export class PageMedia {
       }).then((localeData) => {
         this.localeData = localeData;
 
-        let listElements = document.querySelectorAll('.media-list__item');
+        return this.page.core.loadIcons('/images/admin').then((icons) => {
+          return icons;
+        });
+      }, (rejectionReason) => {
+        this.page.showPopupNotification(rejectionReason, 0);
+      }).then((icons) => {
+        this.icons = icons;
+        this.initUploaderInput(mediaUploaderInput);
+
+        const listElements = document.querySelectorAll('.media-list__item');
         for (let listElement of listElements) {
           this.initMediaElement(listElement);
         }
 
         this.buttons.upload = new Interactive('button');
         this.buttons.upload.target.setLabel(this.localeData.BUTTON_UPLOAD_LABEL);
+
         this.buttons.upload.target.setCallback((event) => {
           event.preventDefault();
           mediaUploaderInput.click();
         });
+
         this.buttons.upload.assembly();
 
         interactiveContainerPagePanelElement.append(this.buttons.upload.target.element);
-      }, (rejectionReason) => {
-        this.page.showPopupNotification(rejectionReason, 0);
       });
     }
   }
