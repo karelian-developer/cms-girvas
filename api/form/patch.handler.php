52ff59d4efa96fcd100ff0ddc6f94f3f8757d3ae
Drelagas <drelagas.new@gmail.com>
2026-03-08 18:03:28 +0300

    0.2.9 Воицы - Исправление мелких ошибок;

--- api/form/patch.handler.php
+++ api/form/patch.handler.php
@@ -49,6 +49,8 @@ if ($CMSCore->client->isLogged(2)) {
         return isset($_PATCH['form_element_name']) && in_array($element['name'], $_PATCH['form_element_name']);
       });
 
+      error_log(print_r($_PATCH, true))
+
       $CMSLocalesNames = $CMSCore->getArrayLocalesNames();
       if (count($CMSLocalesNames) > 0) {
         foreach ($CMSLocalesNames as $index => $name) {
