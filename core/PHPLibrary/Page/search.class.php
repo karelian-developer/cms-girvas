c7e9021494235760c8e49a9201500cf5e6b26cbd
Drelagas <drelagas.new@gmail.com>
2026-04-19 22:06:49 +0300

    0.2.9 Воицы - в класс entries добавлен метод search() для полнотекстового поиска с учётом локализации - в класс entries добавлен метод getcountbysearch() для получения количества найденных записей - реализовано ранжирование результатов поиска по релевантности с настраиваемыми весами полей - в класс pagesearch добавлена поддержка динамического поискового запроса из url-параметра value - исправлено форматирование методов согласно psr-12

---
+++ core/PHPLibrary/Page/search.class.php
@@ -0,0 +1,443 @@
+<?php
+
+/**
+ * CMS «ГИРВАС»
+ * 
+ * Включена в Реестр российского программного обеспечения Минцифры РФ
+ * Реестровый номер: №25012 от 27.11.2024
+ * 
+ * @link        https://gitflic.ru/project/garbalo/cms-girvas Репозиторий продукта
+ * @link        https://cms-girvas.ru Сайт продукта
+ * 
+ * @copyright   Copyright (c) 2021 - 2026, ИП Шестаков А.Р., «Карельский разработчик» (https://карельский-разработчик.рф/)
+ * Все права защищены.
+ * 
+ * @license     https://gitflic.ru/project/garbalo/cms-girvas/LICENSE.md
+ * @author      Андрей Шестаков <andrey.shestakov@karelian-developer.ru>
+ * 
+ * @support     support@karelian-developer.ru
+ */
+
+namespace core\PHPLibrary\Page;
+
+use \core\PHPLibrary\InterfacePage as InterfacePage;
+use \core\PHPLibrary\SystemCore as CMSCore;
+use \core\PHPLibrary\SystemCore\Locale as CMSLocale;
+use \core\PHPLibrary\Page as Page;
+use \core\PHPLibrary\Pagination as Pagination;
+use \core\PHPLibrary\Parsedown as Parsedown;
+use \core\PHPLibrary\Entries as Entries;
+use \core\PHPLibrary\Entry as Entry;
+use \core\PHPLibrary\EntryCategory as EntryCategory;
+use \core\PHPLibrary\Template\Collector as ThemeCollector;
+use \core\PHPLibrary\User as User;
+
+class PageSearch implements InterfacePage
+{
+  public CMSCore $CMSCore;
+  public Page $page;
+  public string $assembled = '';
+
+  /**
+   * __construct
+   *
+   * @param  CMSCore $CMSCore
+   * @param  Page $page
+   * 
+   * @return void
+   */
+  public function __construct(CMSCore $CMSCore, Page $page)
+  {
+    $this->CMSCore = $CMSCore;
+    $this->page = $page;
+  }
+
+  /**
+   * Добавление обязательных CSS-файлов
+   * 
+   * @return void
+   */
+  private function addRequiredStyles() : void
+  {
+    foreach (['page.css', 'page/search.css'] as $stylePath) {
+      $this->CMSCore->theme->addStyle(
+        [
+          'href' => 'styles/' . $stylePath,
+          'rel' => 'stylesheet'
+        ]
+      );
+    }
+  }
+
+  /**
+   * Проверка возможности отображения записи для пользователя
+   * 
+   * Объект User должен передаваться с инициализированными данными:
+   * - metadata
+   * 
+   * @param bool $isPublished
+   * @param ?User $user
+   * 
+   * @return bool
+   */
+  public function entryIsVisible(bool $isPublished, ?User $user) : bool
+  {
+    if ($isPublished && $user !== null) {
+      $userGroup = $user->getGroup();
+
+      if ($userGroup !== null) {
+        return $user->isSuperAdmin()
+          || $userGroup->isSuperGroup()
+          || $userGroup->hasPermissionEditorEntriesEdit();
+      }
+    }
+
+    return false;
+  }
+  
+  /**
+   * Сборка шаблона страницы
+   *
+   * @return void
+   */
+  public function assembly() : void
+  {
+    $this->addRequiredStyles();
+
+    $localeData = $this->CMSCore->locale->getData();
+    $localeName = $this->CMSCore->locale->getName();
+
+    $categoryName = $this->CMSCore->urlp->getPath(1) !== null ? urldecode($this->CMSCore->urlp->getPath(1)) : 'all';
+    
+    if (EntryCategory::existsByName($this->CMSCore, $categoryName) || $categoryName === 'all') {
+      http_response_code(200);
+
+      $pageIndex = $this->CMSCore->urlp->getParam('pageNumber');
+      $searchValue = $this->CMSCore->urlp->getParam('value');
+      $searchValue = htmlspecialchars($searchValue, ENT_QUOTES, 'UTF-8');
+
+      $entriesCountOnPage = 6;
+      $paginationItemCurrent = $pageIndex ?? 0;
+      $paginationItemCurrent = is_numeric($paginationItemCurrent) ? (int) $paginationItemCurrent : 0;
+
+      $this->page->breadcrumbs->add($localeData['PAGE_ENTRIES_BREADCRUMPS_ALL_ENTRIES_LABEL'], '/entries');
+
+      $clientIsLogged = $this->CMSCore->client->isLogged(1);
+      
+      $clientUser = $clientIsLogged ? $this->CMSCore->client->getUser(1) : null;
+      if ($clientUser !== null) {
+        $clientUser->initData(['metadata']);
+      }
+
+      if ($clientUser !== null) {
+        $isPublished = $clientUser->getID() !== 1 || $clientUser->getGroupID() !== 1;
+      } else {
+        $isPublished = true;
+      }
+
+      $this->page->breadcrumbs->assembly();
+
+      $this->CMSCore->configurator->setMetaTitle($localeData['PAGE_SEARCH_BREADCRUMPS_SEARCHING_ENTRIES_LABEL'] . ' | ' . $this->CMSCore->configurator->getSiteTitle());
+
+      /** @var Entries $entries Объект класса Entries */
+      $entries = new Entries($this->CMSCore);
+
+      $entriesObjects = $entries->search($searchValue, $localeName, [
+        'limit' => [$entriesCountOnPage, $paginationItemCurrent * $entriesCountOnPage]
+      ], $isPublished);
+
+      $entriesCount = $entries->getCountBySearch($searchValue, $localeName, $isPublished);
+
+      unset($entries);
+
+      $entriesArrayTemplates = [];
+      $entriesTemplateContent = ThemeCollector::getTemplateFileContent(
+        $this->CMSCore->theme,
+        'templates/page/search/entriesList/item.tpl'
+      );
+
+      $templatesAssembled = [];
+      foreach ($entriesObjects as $entryObject) {
+        $entryObject->initData(['id', 'categoryID', 'texts', 'name', 'createdUnixTimestamp', 'updatedUnixTimestamp', 'metadata']);
+        
+        if ($entryObject->getPublishedUnixTimestamp() > time()) {
+          continue;
+        }
+
+        $entryCategory = $entryObject->getCategory();
+            
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_CATEGORY_TITLE')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_CATEGORY_TITLE',
+            $entryCategory->getTitle($localeName)
+          );
+        }
+
+        $entryCreatedUnixTimestamp = $entryObject->getCreatedUnixTimestamp();
+        $entryPublishedUnixTimestamp = $entryObject->getPublishedUnixTimestamp();
+        $entryUpdatedUnixTimestamp = $entryObject->getUpdatedUnixTimestamp();
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_CREATED_DATE_TIMESTAMP')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_CREATED_DATE_TIMESTAMP',
+            date('d.m.Y H:i:s', $entryCreatedUnixTimestamp)
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_CREATED_DATE_TIMESTAMP')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_PUBLISHED_DATE_TIMESTAMP',
+            $entryPublishedUnixTimestamp > 0
+              ? date('d.m.Y H:i:s', $entryPublishedUnixTimestamp)
+              : '-'
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_CREATED_DATE_TIMESTAMP')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_UPDATED_DATE_TIMESTAMP',
+            date('d.m.Y H:i:s', $entryUpdatedUnixTimestamp)
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_CREATED_DATE_TIMESTAMP_WITHOUT_TIME')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_CREATED_DATE_TIMESTAMP_WITHOUT_TIME',
+            date('d.m.Y', $entryCreatedUnixTimestamp)
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_PUBLISHED_DATE_TIMESTAMP_WITHOUT_TIME')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_PUBLISHED_DATE_TIMESTAMP_WITHOUT_TIME',
+            $entryPublishedUnixTimestamp > 0
+              ? date('d.m.Y', $entryPublishedUnixTimestamp)
+              : '-'
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_UPDATED_DATE_TIMESTAMP_WITHOUT_TIME')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_UPDATED_DATE_TIMESTAMP_WITHOUT_TIME',
+            date('d.m.Y', $entryUpdatedUnixTimestamp)
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_CREATED_DATE_TIMESTAMP_WITHOUT_DATE')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_CREATED_DATE_TIMESTAMP_WITHOUT_DATE',
+            date('H:i:s', $entryCreatedUnixTimestamp)
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_PUBLISHED_DATE_TIMESTAMP_WITHOUT_DATE')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_PUBLISHED_DATE_TIMESTAMP_WITHOUT_DATE',
+            $entryPublishedUnixTimestamp > 0
+              ? date('H:i:s', $entryPublishedUnixTimestamp)
+              : '-'
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_UPDATED_DATE_TIMESTAMP_WITHOUT_DATE')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_UPDATED_DATE_TIMESTAMP_WITHOUT_DATE',
+            date('H:i:s', $entryUpdatedUnixTimestamp)
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_CREATED_DATE_TIMESTAMP_ISO_8601')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_CREATED_DATE_TIMESTAMP_ISO_8601',
+            date('Y-m-dH:i:s', $entryCreatedUnixTimestamp)
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_PUBLISHED_DATE_TIMESTAMP_ISO_8601')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_PUBLISHED_DATE_TIMESTAMP_ISO_8601',
+            $entryPublishedUnixTimestamp > 0
+              ? date('Y-m-dH:i:s', $entryPublishedUnixTimestamp)
+              : '-'
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_UPDATED_DATE_TIMESTAMP_ISO_8601')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_UPDATED_DATE_TIMESTAMP_ISO_8601',
+            date('Y-m-dH:i:s', $entryUpdatedUnixTimestamp)
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_CREATED_DATE_TIMESTAMP_ISO_8601_WITHOUT_TIME')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_CREATED_DATE_TIMESTAMP_ISO_8601_WITHOUT_TIME',
+            date('Y-m-d', $entryCreatedUnixTimestamp)
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_PUBLISHED_DATE_TIMESTAMP_ISO_8601_WITHOUT_TIME')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_PUBLISHED_DATE_TIMESTAMP_ISO_8601_WITHOUT_TIME',
+            $entryPublishedUnixTimestamp > 0
+              ? date('Y-m-d', $entryPublishedUnixTimestamp)
+              : '-'
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_UPDATED_DATE_TIMESTAMP_ISO_8601_WITHOUT_TIME')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_UPDATED_DATE_TIMESTAMP_ISO_8601_WITHOUT_TIME',
+            date('Y-m-d', $entryUpdatedUnixTimestamp)
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_CREATED_DATE_TIMESTAMP_ISO_8601_WITHOUT_DATE')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_CREATED_DATE_TIMESTAMP_ISO_8601_WITHOUT_DATE',
+            date('H:i:s', $entryCreatedUnixTimestamp)
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_PUBLISHED_DATE_TIMESTAMP_ISO_8601_WITHOUT_DATE')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_PUBLISHED_DATE_TIMESTAMP_ISO_8601_WITHOUT_DATE',
+            $entryPublishedUnixTimestamp > 0
+              ? date('H:i:s', $entryPublishedUnixTimestamp)
+              : '-'
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_UPDATED_DATE_TIMESTAMP_ISO_8601_WITHOUT_DATE')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_UPDATED_DATE_TIMESTAMP_ISO_8601_WITHOUT_DATE',
+            date('H:i:s', $entryUpdatedUnixTimestamp)
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_ID')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_ID',
+            $entryObject->getID()
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_NAME')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_NAME',
+            $entryObject->getName()
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_TITLE')) {
+          $value = $entryObject !== null ? $entryObject->getTitle($localeName) : '';
+
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_TITLE',
+            str_replace(
+              ThemeCollector::DECODED_ENTITIES,
+              ThemeCollector::SAFE_SYMBOLS,
+              htmlspecialchars($value, ENT_QUOTES, 'UTF-8')
+            )
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_DESCRIPTION')) {
+          $value = $entryObject !== null ? $entryObject->getDescription($localeName) : '';
+
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_DESCRIPTION',
+            str_replace(
+              ThemeCollector::DECODED_ENTITIES,
+              ThemeCollector::SAFE_SYMBOLS,
+              htmlspecialchars($value, ENT_QUOTES, 'UTF-8')
+            )
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_URL')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_URL',
+            $entryObject->getURL()
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_PREVIEW_URL')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_PREVIEW_URL',
+            $entryObject->getPreviewURL() !== '' ? $entryObject->getPreviewURL() : Entry::getPreviewDefaultURL($this->CMSCore, 512)
+          );
+        }
+
+        if (ThemeCollector::existsTemplateVariable($entriesTemplateContent, 'ENTRY_CATEGORY_URL')) {
+          ThemeCollector::addTemplateVariable(
+            $templatesAssembled,
+            'ENTRY_CATEGORY_URL',
+            $entryCategory->getURL()
+          );
+        }
+
+        if (isset($templatesAssembled['ENTRY_TITLE']) && isset($templatesAssembled['ENTRY_DESCRIPTION'])) {
+          $entriesArrayTemplates[] = ThemeCollector::assemblyFileContent(
+            $this->CMSCore->theme,
+            'templates/page/search/entriesList/item.tpl',
+            $templatesAssembled
+          );
+        }
+
+        $templatesAssembled = [];
+      }
+
+      unset($entriesObjects);
+
+      $pagination = new Pagination($this->CMSCore, $entriesCount, $entriesCountOnPage, $paginationItemCurrent);
+      $pagination->assembly();
+
+      $this->assembled = ThemeCollector::assemblyFileContent($this->CMSCore->theme, 'templates/page.tpl', [
+        'PAGE_NAME' => 'search',
+        'PAGE_CONTENT' => ThemeCollector::assemblyFileContent($this->CMSCore->theme, 'templates/page/search.tpl', [
+          'PAGE_BREADCRUMPS' => $this->page->breadcrumbs->assembled,
+          'ENTRIES_CATEGORY_TITLE' => ($categoryName == 'all') ? $localeData['PAGE_ENTRIES_BREADCRUMPS_ALL_ENTRIES_LABEL'] : $category->getTitle($localeName),
+          'ENTRIES_CATEGORY_DESCRIPTION' => ($categoryName == 'all') ? $localeData['PAGE_ENTRIES_BREADCRUMPS_ALL_ENTRIES_DESCRIPTION'] : $category->getDescription($localeName),
+          'ENTRIES_LIST' => (!empty($entriesArrayTemplates)) ? ThemeCollector::assemblyFileContent($this->CMSCore->theme, 'templates/page/search/entriesList/list.tpl', [
+            'ENTRIES_LIST_ITEMS' => implode($entriesArrayTemplates)
+          ]) : sprintf('<div class="page__simple-note">%s</div>', $localeData['PAGE_ENTRIES_NOT_FOUND_LABEL']),
+          'ENTRIES_PAGINATION' => (!empty($entriesArrayTemplates)) ? $pagination->assembled : ''
+        ])
+      ]);
+
+      unset($entriesArrayTemplates);
+    } else {
+      http_response_code(404);
+
+      $pageError = new PageError($this->CMSCore, $this->page, 404);
+      $pageError->assembly();
+      $this->assembled = $pageError->assembled;
+    }
+  }
+}
--- core/PHPLibrary/entries.class.php
+++ core/PHPLibrary/entries.class.php
@@ -89,6 +89,140 @@ final class Entries
 
     return $entries;
   }
+
+  /**
+   * Поиск записей по строке запроса с учётом локализации
+   *
+   * @param string $searchQuery Строка поискового запроса
+   * @param string $localeName Имя локали (например: 'ru_RU', 'en_US')
+   * @param array $params Дополнительные параметры (limit, offset)
+   * @param bool $isPublished Только опубликованные записи
+   * 
+   * @return array Массив объектов Entry, отсортированных по релевантности
+   */
+  public function search(
+    string $searchQuery,
+    string $localeName = 'en_US',
+    array $params = [],
+    bool $isPublished = false
+  ) : array {
+    $CMSConfigurator = $this->CMSCore->configurator;
+    $CMSConfigDatabase = $CMSConfigurator->get('database');
+    
+    $queryBuilder = new DatabaseQueryBuilder($this->CMSCore, $CMSConfigDatabase['dms']);
+    $queryBuilder->setStatementSelect();
+    
+    // Вес для различных полей при поиске
+    $weights = [
+      'title' => 10,
+      'SEOTitle' => 8,
+      'description' => 5,
+      'SEODescription' => 5,
+      'content' => 3
+    ];
+    
+    $caseExpressions = [];
+    
+    foreach ($weights as $field => $weight) {
+      $jsonPath = sprintf("texts->'%s'->>'%s'", $localeName, $field);
+      $caseExpressions[] = $queryBuilder->createCase()
+        ->whenJsonLike($jsonPath, 'searchQuery', $weight)
+        ->else(0);
+    }
+    
+    // Поиск по ключевым словам (вес 6)
+    $keywordsPath = sprintf("texts->'%s'->'keywords'", $localeName);
+    $caseExpressions[] = $queryBuilder->createCase()
+      ->whenJsonArrayContains($keywordsPath, 'searchQuery', 6)
+      ->else(0);
+    
+    $relevanceExpression = CaseExpression::sum($caseExpressions, 'relevance');
+    
+    $queryBuilder->statement->addSelections(['*', $relevanceExpression]);
+    
+    $queryBuilder->statement->setClauseFrom();
+    $queryBuilder->statement->clauseFrom->addTable('entries');
+    $queryBuilder->statement->clauseFrom->assembly();
+    
+    $whereConditions = [];
+    
+    foreach (array_keys($weights) as $field) {
+      $whereConditions[] = match ($CMSConfigDatabase['dms']) {
+        CMSDMS::PostgreSQL => sprintf(
+          "texts->'%s'->>'%s' ILIKE '%%' || :searchQuery || '%%'",
+          $localeName,
+          $field
+        ),
+        CMSDMS::MySQL => sprintf(
+          "JSON_UNQUOTE(JSON_EXTRACT(texts, '$.%s.%s')) LIKE CONCAT('%%', :searchQuery, '%%')",
+          $localeName,
+          $field
+        )
+      };
+    }
+    
+    $whereConditions[] = match ($CMSConfigDatabase['dms']) {
+      CMSDMS::PostgreSQL => sprintf(
+        "EXISTS (SELECT 1 FROM jsonb_array_elements_text(texts->'%s'->'keywords') AS kw WHERE kw ILIKE '%%' || :searchQuery || '%%')",
+        $localeName
+      ),
+      CMSDMS::MySQL => sprintf(
+        "JSON_SEARCH(texts, 'one', :searchQuery, NULL, '$.%s.keywords[*]') IS NOT NULL",
+        $localeName
+      )
+    };
+    
+    $whereString = '(' . implode(' OR ', $whereConditions) . ')';
+    
+    $queryBuilder->statement->setClauseWhere();
+    $queryBuilder->statement->clauseWhere->addCondition($whereString);
+    
+    if ($isPublished) {
+      $queryBuilder->statement->clauseWhere->addConditionAdaptive([
+        'mysql' => 'AND JSON_EXTRACT(`metadata`, \'$.isPublished\') = 1',
+        'postgresql' => 'AND (metadata::jsonb->>\'isPublished\')::boolean = true'
+      ]);
+    }
+    
+    $queryBuilder->statement->clauseWhere->assembly();
+    
+    $queryBuilder->statement->setClauseOrderBy();
+    $queryBuilder->statement->clauseOrderBy->setColumn('relevance');
+    $queryBuilder->statement->clauseOrderBy->setSortType('DESC');
+    $queryBuilder->statement->clauseOrderBy->assembly();
+    
+    if (array_key_exists('limit', $params)) {
+      if (is_array($params['limit'])) {
+        $limit = is_integer($params['limit'][0]) ? $params['limit'][0] : 0;
+        $offset = is_integer($params['limit'][1]) ? $params['limit'][1] : 0;
+        $queryBuilder->statement->setClauseLimit($limit, $offset);
+      }
+    }
+    
+    $queryBuilder->statement->assembly();
+    
+    // Модифицируем ORDER BY для поддержки второй колонки (id)
+    $queryBuilder->statement->assembled = str_replace(
+      'ORDER BY "relevance" DESC',
+      'ORDER BY relevance DESC, id',
+      $queryBuilder->statement->assembled
+    );
+    
+    $databaseConnection = $this->CMSCore->databaseConnector->database->connection;
+    $databaseQuery = $databaseConnection->prepare($queryBuilder->statement->assembled);
+    $databaseQuery->bindParam(':searchQuery', $searchQuery, \PDO::PARAM_STR);
+    $databaseQuery->execute();
+    
+    $entries = [];
+    $results = $databaseQuery->fetchAll(\PDO::FETCH_ASSOC);
+    if ($results) {
+      foreach ($results as $data) {
+        $entries[] = new Entry($this->CMSCore, $data['id']);
+      }
+    }
+    
+    return $entries;
+  }
       
   /**
    * Получить объекты записей для определенной категории
@@ -148,6 +282,86 @@ final class Entries
     }
 
     return $entries;
+  }
+
+  /**
+   * Получить количество записей по поисковому запросу
+   *
+   * @param string $searchQuery Строка поискового запроса
+   * @param string $localeName Имя локали (например: 'ru_RU', 'en_US')
+   * @param bool $isPublished Только опубликованные записи
+   * 
+   * @return int Количество найденных записей
+   */
+  public function getCountBySearch(
+    string $searchQuery,
+    string $localeName = 'en_US',
+    bool $isPublished = false
+  ) : int {
+    $CMSConfigurator = $this->CMSCore->configurator;
+    $CMSConfigDatabase = $CMSConfigurator->get('database');
+    
+    $queryBuilder = new DatabaseQueryBuilder($this->CMSCore, $CMSConfigDatabase['dms']);
+    $queryBuilder->setStatementSelect();
+    
+    $queryBuilder->statement->addSelections(['count(*) AS count']);
+    
+    $queryBuilder->statement->setClauseFrom();
+    $queryBuilder->statement->clauseFrom->addTable('entries');
+    $queryBuilder->statement->clauseFrom->assembly();
+    
+    $weights = ['title', 'SEOTitle', 'description', 'SEODescription', 'content'];
+    
+    $whereConditions = [];
+    
+    foreach ($weights as $field) {
+      $whereConditions[] = match ($CMSConfigDatabase['dms']) {
+        CMSDMS::PostgreSQL => sprintf(
+          "texts->'%s'->>'%s' ILIKE '%%' || :searchQuery || '%%'",
+          $localeName,
+          $field
+        ),
+        CMSDMS::MySQL => sprintf(
+          "JSON_UNQUOTE(JSON_EXTRACT(texts, '$.%s.%s')) LIKE CONCAT('%%', :searchQuery, '%%')",
+          $localeName,
+          $field
+        )
+      };
+    }
+    
+    $whereConditions[] = match ($CMSConfigDatabase['dms']) {
+      CMSDMS::PostgreSQL => sprintf(
+        "EXISTS (SELECT 1 FROM jsonb_array_elements_text(texts->'%s'->'keywords') AS kw WHERE kw ILIKE '%%' || :searchQuery || '%%')",
+        $localeName
+      ),
+      CMSDMS::MySQL => sprintf(
+        "JSON_SEARCH(texts, 'one', :searchQuery, NULL, '$.%s.keywords[*]') IS NOT NULL",
+        $localeName
+      )
+    };
+    
+    $whereString = '(' . implode(' OR ', $whereConditions) . ')';
+    
+    $queryBuilder->statement->setClauseWhere();
+    $queryBuilder->statement->clauseWhere->addCondition($whereString);
+    
+    if ($isPublished) {
+      $queryBuilder->statement->clauseWhere->addConditionAdaptive([
+        'mysql' => 'AND JSON_EXTRACT(`metadata`, \'$.isPublished\') = 1',
+        'postgresql' => 'AND (metadata::jsonb->>\'isPublished\')::boolean = true'
+      ]);
+    }
+    
+    $queryBuilder->statement->clauseWhere->assembly();
+    $queryBuilder->statement->assembly();
+    
+    $databaseConnection = $this->CMSCore->databaseConnector->database->connection;
+    $databaseQuery = $databaseConnection->prepare($queryBuilder->statement->assembled);
+    $databaseQuery->bindParam(':searchQuery', $searchQuery, \PDO::PARAM_STR);
+    $databaseQuery->execute();
+    
+    $result = $databaseQuery->fetch(\PDO::FETCH_ASSOC);
+    return $result ? (int)$result['count'] : 0;
   }
       
   /**
