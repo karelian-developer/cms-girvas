8bd5f6709bb5edbcc7006b761b9b8d5e4480f961
Drelagas <drelagas.new@gmail.com>
2026-07-03 22:20:54 +0300

    0.3.1 Шуя - исправление мелких ошибок;

--- core/JSLibrary/nadvoTE.class.js
+++ core/JSLibrary/nadvoTE.class.js
@@ -1,28 +1,12 @@
-/**
- * CMS «ГИРВАС»
- * 
- * Включена в Реестр российского программного обеспечения Минцифры РФ.
- * Реестровый номер: №25012 от 27.11.2024
- * 
- * @copyright Copyright (c) 2021 - 2026, ИП Шестаков А.Р., «Карельский разработчик».
- *             Все права защищены.
- * @license   https://gitflic.ru/project/garbalo/cms-girvas/LICENSE.md
- * @see       https://gitflic.ru/project/garbalo/cms-girvas Репозиторий продукта
- * @see       https://cms-girvas.ru Сайт продукта
- * @author    Андрей Шестаков <andrey.shestakov@karelian-developer.ru>
- * @support   support@karelian-developer.ru
- */
-
-'use strict';
-
-import {Toolbar} from './nadvoTE/toolbar.class.js';
-import {Textarea} from './nadvoTE/textarea.class.js';
-import {TextareaVisual} from './nadvoTE/textareaVisual.class.js';
-
 export class NadvoTE {
   constructor(element, options = {}) {
     this.element = element;
     this.options = options;
+    this._savedSelection = {
+      text: '',
+      start: 0,
+      end: 0
+    };
     console.log(`[NADVO TE] Object created.`);
   }
 
@@ -35,6 +19,11 @@ export class NadvoTE {
     this.element.appendChild(this.toolbar.element);
     this.element.appendChild(this.textarea.element);
     this.element.appendChild(this.textareaVisual.element);
+
+    // Сохраняем выделение при уходе фокуса с textarea
+    this.textarea.element.addEventListener('blur', () => {
+      this.saveSelection();
+    });
 
     const copyright = this.createElementDiv();
     copyright.classList.add('nadvo-te__copyright');
@@ -57,6 +46,38 @@ export class NadvoTE {
     textareaVisual.init();
   }
 
+  // Новый метод для сохранения выделения
+  saveSelection() {
+    const textarea = this.textarea.element;
+    this._savedSelection.text = textarea.value.substring(
+      textarea.selectionStart, 
+      textarea.selectionEnd
+    );
+    this._savedSelection.start = textarea.selectionStart;
+    this._savedSelection.end = textarea.selectionEnd;
+  }
+
+  // Обновлённый метод получения выделенного текста
+  getSelectionString() {
+    const textarea = this.textarea.element;
+    
+    // Если textarea в фокусе, берём текущее выделение
+    if (document.activeElement === textarea) {
+      return textarea.value.substring(
+        textarea.selectionStart, 
+        textarea.selectionEnd
+      );
+    }
+    
+    // Иначе используем сохранённое выделение
+    return this._savedSelection.text;
+  }
+
+  async fetchJSON(url, data) {
+    return fetch(url, data).then(response => response.ok ? response.json() : Promise.reject(response));
+  }
+  
+  // Остальные методы без изменений
   createElementTextarea() {
     return document.createElement('textarea');
   }
@@ -80,15 +101,6 @@ export class NadvoTE {
   createElementButton(content) {
     let element = document.createElement('button');
     element.innerHTML = content;
-
     return element;
-  }
-  
-  getSelectionString() {
-    return window.getSelection().toString();
-  }
-
-  async fetchJSON(url, data) {
-    return fetch(url, data).then(response => response.ok ? response.json() : Promise.reject(response));
   }
 }
--- core/JSLibrary/nadvoTE/toolbar/toolHeaders.class.js
+++ core/JSLibrary/nadvoTE/toolbar/toolHeaders.class.js
@@ -1,22 +1,3 @@
-/**
- * CMS «ГИРВАС»
- * 
- * Включена в Реестр российского программного обеспечения Минцифры РФ.
- * Реестровый номер: №25012 от 27.11.2024
- * 
- * @copyright Copyright (c) 2021 - 2026, ИП Шестаков А.Р., «Карельский разработчик».
- *             Все права защищены.
- * @license   https://gitflic.ru/project/garbalo/cms-girvas/LICENSE.md
- * @see       https://gitflic.ru/project/garbalo/cms-girvas Репозиторий продукта
- * @see       https://cms-girvas.ru Сайт продукта
- * @author    Андрей Шестаков <andrey.shestakov@karelian-developer.ru>
- * @support   support@karelian-developer.ru
- */
-
-'use strict';
-
-import {Tool} from './tool.class.js';
-
 export class ToolHeaders extends Tool {
   constructor(editor, element) {
     super(editor, {
@@ -25,53 +6,26 @@ export class ToolHeaders extends Tool {
       iconPath: '',
       element: element
     });
-
-    this.savedSelection = {
-      text: '',
-      start: 0,
-      end: 0
-    };
   }
 
   initClickEvent() {
-    const selectElement = this.element.querySelector('select');
-    const textarea = this.editor.textarea.element;
-    
-    // Сохраняем выделение при клике на select (срабатывает до потери фокуса)
-    selectElement.addEventListener('mousedown', () => {
-      this.savedSelection.text = textarea.value.substring(
-        textarea.selectionStart, 
-        textarea.selectionEnd
-      );
-      this.savedSelection.start = textarea.selectionStart;
-      this.savedSelection.end = textarea.selectionEnd;
-    });
-    
-    // При изменении select используем сохранённое выделение
-    selectElement.addEventListener('change', (event) => {
-      event.preventDefault();
+    super.addChangeEvent(() => {
+      console.log(`[NADVO TE] Tool ${this.name} selected!`);
+      const selectElement = this.element.querySelector('select');
       
-      const headerLevel = selectElement.value;
+      // Возвращаем фокус в textarea
+      this.editor.textarea.element.focus();
       
-      if (headerLevel && this.savedSelection.text) {
-        // Возвращаем фокус и восстанавливаем выделение
-        textarea.focus();
-        textarea.setSelectionRange(
-          this.savedSelection.start, 
-          this.savedSelection.end
-        );
-        
-        // Вставляем заголовок
+      const selectedText = this.editor.getSelectionString();
+      
+      if (selectedText) {
         this.editor.textarea.replaceStringSelection(
-          '#'.repeat(headerLevel) + ' ' + this.savedSelection.text
+          '#'.repeat(selectElement.value) + ' ' + selectedText
         );
-        
-        // Сбрасываем select
-        selectElement.selectedIndex = 0;
-        
-        // Очищаем сохранённое
-        this.savedSelection.text = '';
       }
+      
+      // Сбрасываем select
+      selectElement.selectedIndex = 0;
     });
   }
 }
