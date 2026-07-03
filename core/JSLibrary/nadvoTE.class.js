19b037081c267e3b7601f7429424bbe857eb8d39
Drelagas <drelagas.new@gmail.com>
2026-07-03 22:21:43 +0300

    0.3.1 Шуя - исправление мелких ошибок;

--- core/JSLibrary/nadvoTE.class.js
+++ core/JSLibrary/nadvoTE.class.js
@@ -1,3 +1,24 @@
+/**
+ * CMS «ГИРВАС»
+ * 
+ * Включена в Реестр российского программного обеспечения Минцифры РФ.
+ * Реестровый номер: №25012 от 27.11.2024
+ * 
+ * @copyright Copyright (c) 2021 - 2026, ИП Шестаков А.Р., «Карельский разработчик».
+ *             Все права защищены.
+ * @license   https://gitflic.ru/project/garbalo/cms-girvas/LICENSE.md
+ * @see       https://gitflic.ru/project/garbalo/cms-girvas Репозиторий продукта
+ * @see       https://cms-girvas.ru Сайт продукта
+ * @author    Андрей Шестаков <andrey.shestakov@karelian-developer.ru>
+ * @support   support@karelian-developer.ru
+ */
+
+'use strict';
+
+import {Toolbar} from './nadvoTE/toolbar.class.js';
+import {Textarea} from './nadvoTE/textarea.class.js';
+import {TextareaVisual} from './nadvoTE/textareaVisual.class.js';
+
 export class NadvoTE {
   constructor(element, options = {}) {
     this.element = element;
