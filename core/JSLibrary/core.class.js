72533c405294180eb29fd78be591edee4c5e656a
Drelagas <drelagas.new@gmail.com>
2026-09-03 11:48:04 +0300

    0.3.1 Шуя - оптимизация кода;

--- core/JSLibrary/core.class.js
+++ core/JSLibrary/core.class.js
@@ -56,7 +56,7 @@ export class Core {
    * @returns
    */
   async initClient() {
-    return this.client.isLogged().then((result) => {
+    return await this.client.isLogged().then((result) => {
       this.client.isLogged = result;
       return this.client.getIPAddress();
     }).then((result) => {
