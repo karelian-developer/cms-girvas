a0f1a570dec8876ad17a98a51a73a7c8494cc0ce
Drelagas <drelagas.new@gmail.com>
2026-09-19 17:23:47 +0300

    0.3.1 Шуя - исправление мелких ошибок;

--- api/user/post.handler.php
+++ api/user/post.handler.php
@@ -17,6 +17,7 @@ use \core\PHPLibrary\Template as Theme;
 use \core\PHPLibrary\Template\Collector as ThemeCollector;
 use \core\PHPLibrary\Mail\SMTPClient as SMTPClient;
 use \core\PHPLibrary\User as User;
+use \core\PHPLibrary\User\Consent as UserConsent;
 use \core\PHPLibrary\UserGroup as UserGroup;
 use \core\PHPLibrary\SystemCore\Report as Report;
 use \core\PHPLibrary\SystemCore\Reports as Reports;
